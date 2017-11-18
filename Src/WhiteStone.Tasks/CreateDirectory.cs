316f1c0072269ee0111a9d7c0eabfa45d2586c3d
test <beyaz1404@hotmail.com>
2017-11-18 13:45:51 +0300

    .

---
+++ Src/WhiteStone.Tasks/CreateDirectory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace WhiteStone.Tasks
+{
+    public class CreateDirectory : ITask
+    {
+        #region Public Properties
+        public IDictionary<string, string> Keys { get; set; }
+        #endregion
+
+        #region Properties
+        string Path => Keys[nameof(Path)];
+        #endregion
+
+        #region Public Methods
+        public void Run()
+        {
+            Directory.CreateDirectory(Path);
+        }
+        #endregion
+    }
+}
