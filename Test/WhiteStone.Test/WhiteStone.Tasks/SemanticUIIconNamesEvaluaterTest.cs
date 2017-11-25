25b53a1b01356d2f90ad416c61fd27fdbf382dbb
test <beyaz1404@hotmail.com>
2017-11-25 23:58:50 +0300

    .

---
+++ Test/WhiteStone.Test/WhiteStone.Tasks/SemanticUIIconNamesEvaluaterTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhiteStone.Tasks
+{
+    [TestClass]
+    public class SemanticUIIconNamesEvaluaterTest
+    {
+        #region Public Methods
+        [TestMethod]
+        public void Run()
+        {
+            var api = new SemanticUIIconNamesEvaluater();
+
+            api.Run();
+        }
+        #endregion
+    }
+}
