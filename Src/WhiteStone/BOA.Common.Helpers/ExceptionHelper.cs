2e60eca40c6731d123e163822c35fb21d78595b3
test <beyaz1404@hotmail.com>
2017-12-05 14:41:22 +0300

    .

---
+++ Src/WhiteStone/BOA.Common.Helpers/ExceptionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BOA.Common.Helpers
+{
+    /// <summary>
+    ///     The exception helper
+    /// </summary>
+    public static class ExceptionHelper
+    {
+        #region Public Methods
+        /// <summary>
+        ///     Adds the data.
+        /// </summary>
+        public static T AddData<T>(this T exception, string key, object value) where T : Exception
+        {
+            exception.Data[key] = value;
+            return exception;
+        }
+        #endregion
+    }
+}
