16d973bc9441c74ff6c0fafb1a9c2af5b9984dd4
test <beyaz1404@hotmail.com>
2018-01-06 20:27:40 +0300

    .

---
+++ Src/WhiteStone/System.Web/WhiteStoneExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System.Web
+{
+    /// <summary>
+    ///     The white stone extensions
+    /// </summary>
+    public static class WhiteStoneExtensions
+    {
+        #region Public Methods
+        /// <summary>
+        ///     Inputs the stream as json.
+        /// </summary>
+        public static string InputStreamAsJson(this HttpRequest request)
+        {
+            string documentContents;
+            using (var receiveStream = request.InputStream)
+            {
+                using (var readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    documentContents = readStream.ReadToEnd();
+                }
+            }
+
+            return documentContents;
+        }
+        #endregion
+    }
+}
