7f6d1802adb4e83e027aca44605708c7e170c167
test <beyaz1404@hotmail.com>
2018-01-11 08:46:21 +0300

    .

--- Src/WhiteStone/Office/ExcelWrapper.cs
+++ Src/WhiteStone/Office/ExcelWrapper.cs
@@ -1,125 +1,127 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using Microsoft.Office.Interop.Excel;
+﻿
+// Microsoft.Office.Interop.Excel, Version=12.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c
+//using System;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Linq;
+//using Microsoft.Office.Interop.Excel;
 
-namespace WhiteStone.Office
-{
-    /// <summary>
-    /// Defines the excel wrapper.
-    /// </summary>
-    /// <seealso cref="System.IDisposable" />
-    public class ExcelWrapper : IDisposable
-    {
-        /// <summary>
-        /// Updates the cell.
-        /// </summary>
-        public void UpdateCell<T>(string sheetName, int rowIndex, int columnIndex, T value)
-        {
-            var mWorkSheets = WorkBook.Worksheets;
-            var mWSheet1 = (Worksheet) mWorkSheets[sheetName];
+//namespace WhiteStone.Office
+//{
+//    /// <summary>
+//    /// Defines the excel wrapper.
+//    /// </summary>
+//    /// <seealso cref="System.IDisposable" />
+//    public class ExcelWrapper : IDisposable
+//    {
+//        /// <summary>
+//        /// Updates the cell.
+//        /// </summary>
+//        public void UpdateCell<T>(string sheetName, int rowIndex, int columnIndex, T value)
+//        {
+//            var mWorkSheets = WorkBook.Worksheets;
+//            var mWSheet1 = (Worksheet) mWorkSheets[sheetName];
 
-            mWSheet1.Cells[rowIndex, columnIndex].Value2 = value;
-        }
-        /// <summary>
-        /// Saves this instance.
-        /// </summary>
-        public void Save()
-        {
-            WorkBook.SaveAs(FilePath);
+//            mWSheet1.Cells[rowIndex, columnIndex].Value2 = value;
+//        }
+//        /// <summary>
+//        /// Saves this instance.
+//        /// </summary>
+//        public void Save()
+//        {
+//            WorkBook.SaveAs(FilePath);
 
-            WorkBook.Close();
-        }
+//            WorkBook.Close();
+//        }
 
-        #region Helper methods
+//        #region Helper methods
 
-        static void KillSpecificExcelFileProcess(string excelFileName)
-        {
-            var processes = from p in Process.GetProcessesByName("EXCEL")
-                select p;
+//        static void KillSpecificExcelFileProcess(string excelFileName)
+//        {
+//            var processes = from p in Process.GetProcessesByName("EXCEL")
+//                select p;
 
-            foreach (var process in processes)
-            {
-                if (process.MainWindowTitle == "Microsoft Excel - " + excelFileName)
-                {
-                    process.Kill();
-                }
-            }
-        }
+//            foreach (var process in processes)
+//            {
+//                if (process.MainWindowTitle == "Microsoft Excel - " + excelFileName)
+//                {
+//                    process.Kill();
+//                }
+//            }
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Properties        
-        /// <summary>
-        /// Gets or sets the file path.
-        /// </summary>
-        public string FilePath { get; set; }
+//        #region Properties        
+//        /// <summary>
+//        /// Gets or sets the file path.
+//        /// </summary>
+//        public string FilePath { get; set; }
 
-        Application _excelApplication;
+//        Application _excelApplication;
 
-        Application ExcelApplication
-        {
-            get
-            {
-                if (_excelApplication == null)
-                {
-                    _excelApplication = new Application();
-                }
-                return _excelApplication;
-            }
-        }
+//        Application ExcelApplication
+//        {
+//            get
+//            {
+//                if (_excelApplication == null)
+//                {
+//                    _excelApplication = new Application();
+//                }
+//                return _excelApplication;
+//            }
+//        }
 
-        Workbook _workbook;
+//        Workbook _workbook;
 
-        Workbook WorkBook
-        {
-            get
-            {
-                if (FilePath == null)
-                {
-                    throw new ArgumentException("FilePath");
-                }
-                if (_workbook == null)
-                {
-                    var processName = Path.GetFileName(FilePath);
+//        Workbook WorkBook
+//        {
+//            get
+//            {
+//                if (FilePath == null)
+//                {
+//                    throw new ArgumentException("FilePath");
+//                }
+//                if (_workbook == null)
+//                {
+//                    var processName = Path.GetFileName(FilePath);
 
-                    KillSpecificExcelFileProcess(processName);
+//                    KillSpecificExcelFileProcess(processName);
 
-                    _workbook = ExcelApplication.Workbooks.Open(FilePath);
-                }
+//                    _workbook = ExcelApplication.Workbooks.Open(FilePath);
+//                }
 
-                return _workbook;
-            }
-        }
+//                return _workbook;
+//            }
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Dispose
+//        #region Dispose
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        /// <summary>
-        /// Releases unmanaged and - optionally - managed resources.
-        /// </summary>
-        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-                GC.Collect();
-            }
-        }
+//        /// <summary>
+//        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+//        /// </summary>
+//        public void Dispose()
+//        {
+//            Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
+//        /// <summary>
+//        /// Releases unmanaged and - optionally - managed resources.
+//        /// </summary>
+//        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                GC.WaitForPendingFinalizers();
+//                GC.Collect();
+//                GC.WaitForPendingFinalizers();
+//                GC.Collect();
+//            }
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}
