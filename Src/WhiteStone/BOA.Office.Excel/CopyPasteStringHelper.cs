0dc987436fcf0d5665238c74f3b72de3765a26be
test <beyaz1404@hotmail.com>
2018-03-01 12:44:13 +0300

    .

---
+++ Src/WhiteStone/BOA.Office.Excel/CopyPasteStringHelper.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using BOA.Common.Helpers;
+
+namespace BOA.Office.Excel
+{
+    /// <summary>
+    ///     The copy paste string helper
+    /// </summary>
+    public class CopyPasteStringHelper
+    {
+        #region Constants
+        const char TabCharachter = '\t';
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        ///     Prepares for paste to excel.
+        /// </summary>
+        public static IReadOnlyList<TContract> ParseFromString<TContract>(string copiedExcelData, IReadOnlyList<string> propertyNames) where TContract : new()
+        {
+            if (copiedExcelData == null)
+            {
+                throw new ArgumentNullException(nameof(copiedExcelData));
+            }
+
+            copiedExcelData = copiedExcelData.Trim();
+
+            var rows = copiedExcelData.Split(Environment.NewLine.ToCharArray()).Where(x => !string.IsNullOrWhiteSpace(x));
+
+            var table = new List<string[]>();
+
+            foreach (var row in rows)
+            {
+                var cells = row.Split(TabCharachter);
+                table.Add(cells);
+            }
+
+            return table.ConvertAll(r => LineToContract<TContract>(r, propertyNames));
+        }
+
+        /// <summary>
+        ///     Prepares for paste to excel.
+        /// </summary>
+        public static string PrepareForPasteToExcel<TContract>(IReadOnlyList<TContract> contracts, IReadOnlyList<string> propertyNames)
+        {
+            var rows = new List<List<string>>();
+
+            foreach (var contract in contracts)
+            {
+                var row = new List<string>();
+
+                foreach (var propertyName in propertyNames)
+                {
+                    var valueAsString = GetPropertyValueForExcelCell(contract, propertyName);
+
+                    row.Add(valueAsString);
+                }
+
+                rows.Add(row);
+            }
+
+            return string.Join(Environment.NewLine, rows.ConvertAll(r => string.Join(TabCharachter.ToString(), r)));
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        ///     Gets the property value for excel cell.
+        /// </summary>
+        static string GetPropertyValueForExcelCell<TContract>(TContract contract, string propertyName)
+        {
+            var value = Reflection.GetPropertyValue(contract, propertyName);
+
+            var valueAsString = value + "";
+
+            return valueAsString;
+        }
+
+        /// <summary>
+        ///     Lines to contract.
+        /// </summary>
+        static TContract LineToContract<TContract>(string[] rowCells, IReadOnlyList<string> propertyNames) where TContract : new()
+        {
+            var contract = new TContract();
+
+            for (var i = 0; i < propertyNames.Count; i++)
+            {
+                var propertyName = propertyNames[i];
+                var cellValue    = rowCells[i];
+
+                var propertyType = Reflection.GetPropertyType(contract, propertyName);
+
+                var propertyValue = Cast.To(cellValue, propertyType, CultureInfo.CurrentCulture);
+
+                Reflection.SetPropertyValue(contract, propertyName, propertyValue);
+            }
+
+            return contract;
+        }
+        #endregion
+
+        /// <summary>
+        ///     The reflection
+        /// </summary>
+        static class Reflection
+        {
+            #region Public Methods
+            /// <summary>
+            ///     Gets the type of the property.
+            /// </summary>
+            public static Type GetPropertyType(object instance, string propertyName)
+            {
+                var propertyInfo = instance.GetType().GetProperty(propertyName);
+                if (propertyInfo == null)
+                {
+                    throw new ArgumentException("PropertyNotFound." + propertyName);
+                }
+
+                return propertyInfo.PropertyType;
+            }
+
+            /// <summary>
+            ///     Gets the property value.
+            /// </summary>
+            public static object GetPropertyValue(object instance, string propertyName)
+            {
+                var propertyInfo = instance.GetType().GetProperty(propertyName);
+                if (propertyInfo == null)
+                {
+                    throw new ArgumentException("PropertyNotFound." + propertyName);
+                }
+
+                return propertyInfo.GetValue(instance);
+            }
+
+            /// <summary>
+            ///     Sets the property value.
+            /// </summary>
+            public static void SetPropertyValue(object instance, string propertyName, object value)
+            {
+                var propertyInfo = instance.GetType().GetProperty(propertyName);
+                if (propertyInfo == null)
+                {
+                    throw new ArgumentException("PropertyNotFound." + propertyName);
+                }
+
+                propertyInfo.SetValue(instance, value);
+            }
+            #endregion
+        }
+    }
+}
---
+++ Test/WhiteStone.Test/BOA.Office.Excel/CopyPasteStringHelperTest.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace BOA.Office.Excel
+{
+    [TestClass]
+    public class CopyPasteStringHelperTest
+    {
+        #region Public Methods
+        [TestMethod]
+        public void PrepareForPasteToExcel()
+        {
+            var items = CreateTestContracts();
+
+            var propertyNames = new[]
+            {
+                nameof(TestClass.DecimalProp0),
+                nameof(TestClass.DecimalProp1),
+
+                nameof(TestClass.StringProp1),
+                nameof(TestClass.StringProp0),
+
+                nameof(TestClass.Int32Prop0),
+                nameof(TestClass.Int32Prop1)
+            };
+
+            var result = CopyPasteStringHelper.PrepareForPasteToExcel(items, propertyNames);
+
+            const string expected = @"56.78	43.02	ye2öçştj	Test6klö	5	6
+56.71	43.03	ye2öçştj_1	Test6klö_1	57	63
+56.72	43.03	ye2öçştj_2_____________ğ__________ü	Test6klö_2	58	65";
+
+            Assert.AreEqual(expected, result);
+
+            var deserializedItems = CopyPasteStringHelper.ParseFromString<TestClass>(expected, propertyNames);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(items), JsonConvert.SerializeObject(deserializedItems));
+        }
+        #endregion
+
+        #region Methods
+        static TestClass[] CreateTestContracts()
+        {
+            return new[]
+            {
+                new TestClass
+                {
+                    DecimalProp0 = 56.78M,
+                    DecimalProp1 = 43.02M,
+                    Int32Prop0   = 5,
+                    Int32Prop1   = 6,
+                    StringProp0  = "Test6klö",
+                    StringProp1  = "ye2öçştj"
+                },
+                new TestClass
+                {
+                    DecimalProp0 = 56.71M,
+                    DecimalProp1 = 43.03M,
+                    Int32Prop0   = 57,
+                    Int32Prop1   = 63,
+                    StringProp0  = "Test6klö_1",
+                    StringProp1  = "ye2öçştj_1"
+                },
+                new TestClass
+                {
+                    DecimalProp0 = 56.72M,
+                    DecimalProp1 = 43.03M,
+                    Int32Prop0   = 58,
+                    Int32Prop1   = 65,
+                    StringProp0  = "Test6klö_2",
+                    StringProp1  = "ye2öçştj_2_____________ğ__________ü"
+                }
+            };
+        }
+        #endregion
+
+        class TestClass
+        {
+            #region Public Properties
+            public decimal? DecimalProp0 { get; set; }
+            public decimal? DecimalProp1 { get; set; }
+
+            public int    Int32Prop0  { get; set; }
+            public int    Int32Prop1  { get; set; }
+            public string StringProp0 { get; set; }
+            public string StringProp1 { get; set; }
+            #endregion
+        }
+    }
+}
