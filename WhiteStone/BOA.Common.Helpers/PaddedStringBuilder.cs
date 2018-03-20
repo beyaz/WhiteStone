f908c04dc5c5654b62bda3886d226c8350f54e2e
test <beyaz1404@hotmail.com>
2018-03-20 12:32:02 +0300

    .

---
+++ WhiteStone/BOA.Common.Helpers/PaddedStringBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+namespace BOA.BOA.Common.Helpers
+{
+    /// <summary>
+    ///     The padded string builder
+    /// </summary>
+    public class PaddedStringBuilder
+    {
+        #region Fields
+        /// <summary>
+        ///     The sb
+        /// </summary>
+        readonly StringBuilder sb = new StringBuilder();
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        ///     Gets or sets the length of the padding.
+        /// </summary>
+        public int PaddingLength { get; set; }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Gets the padding.
+        /// </summary>
+        string Padding
+        {
+            get { return "".PadRight(PaddingLength); }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        ///     Appends the line.
+        /// </summary>
+        public void AppendLine(string line)
+        {
+            sb.Append(Padding);
+            sb.AppendLine(line);
+        }
+
+        /// <summary>
+        ///     Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
