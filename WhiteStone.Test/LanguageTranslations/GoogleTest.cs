625628c56dcc19733f93a205c85e5fe9763719da
test <beyaz1404@hotmail.com>
2018-07-17 12:06:00 +0300

    .

--- WhiteStone.Test/LanguageTranslations/GoogleTest.cs
+++ WhiteStone.Test/LanguageTranslations/GoogleTest.cs
@@ -1,20 +1,20 @@
-﻿//using System.Collections.Generic;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using WhiteStone.Helpers;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WhiteStone.Helpers;
 
-//namespace BOA.LanguageTranslations
-//{
-//    [TestClass]
-//    public class GoogleTest
-//    {
-//        [TestMethod]
-//        public void TranslateEnglishToTurkish()
-//        {
-//            var wordInfo = Google.TranslateEnglishToTurkish("mean");
+namespace BOA.LanguageTranslations
+{
+    [TestClass]
+    public class TurkishToEnglishTranslatorTest
+    {
+        [TestMethod]
+        public void TranslateEnglishToTurkish()
+        {
+            var wordInfo = Google.TranslateEnglishToTurkish("mean");
 
-//            Assert.AreEqual("anlamına gelmek", wordInfo.Definition);
-//        }
+            Assert.AreEqual("anlamına gelmek", wordInfo.Definition);
+        }
 
-        
-//    }
-//}
+
+    }
+}
