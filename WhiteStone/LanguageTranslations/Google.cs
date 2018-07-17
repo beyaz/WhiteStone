e7108ca2731fc285f366c2f5ee94faa06fc44054
test <beyaz1404@hotmail.com>
2018-07-17 08:25:23 +0300

    .

--- WhiteStone/LanguageTranslations/Google.cs
+++ WhiteStone/LanguageTranslations/Google.cs
@@ -1,26 +1,21 @@
-﻿//using System.Linq;
-//using HtmlAgilityPack;
+﻿using RavSoft.GoogleTranslator;
 
-//namespace BOA.LanguageTranslations
-//{
-//    class Google
-//    {
-//        public static WordInfo TranslateEnglishToTurkish(string word)
-//        {
-
-
-//            var url = "https://translate.google.com/#en/tr/mean";
-//            var web = new HtmlWeb();
-//            var doc = web.Load(url, "10.13.50.100",8080,"beyaztas","hattori_hanzo_41");
-
-//            var result = doc.DocumentNode.SelectNodes("//*[@id='result_box']")?.FirstOrDefault();
-
-           
-
-//            return new WordInfo
-//            {
-//                Definition = result?.InnerHtml
-//            };
-//        }
-//    }
-//}
+namespace BOA.LanguageTranslations
+{
+    class Google
+    {
+        #region Public Methods
+        public static string TranslateTurkishToEnglish(string sourceText)
+        {
+            var t = new Translator
+            {
+                SourceLanguage = "Turkish",
+                TargetLanguage = "English",
+                SourceText     = sourceText
+            };
+            t.Translate();
+            return t.Translation;
+        }
+        #endregion
+    }
+}
