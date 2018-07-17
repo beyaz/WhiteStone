b128a27c3e9c92c97c99529fd5c2b2dd7183b2de
test <beyaz1404@hotmail.com>
2018-07-17 12:07:45 +0300

    .

---
+++ WhiteStone.Test/LanguageTranslations/Google/TurkishToEnglishTranslatorTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.LanguageTranslations.Google
+{
+    [TestClass]
+    public class TurkishToEnglishTranslatorTest
+    {
+        #region Public Methods
+        [TestMethod]
+        public void TranslateEnglishToTurkish()
+        {
+            var english = TurkishToEnglishTranslator.Translate("araba");
+
+            Assert.AreEqual("car", english);
+        }
+        #endregion
+    }
+}
