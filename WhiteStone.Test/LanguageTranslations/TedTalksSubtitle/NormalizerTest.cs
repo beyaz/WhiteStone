8edcec9cd63579c7cb91e21b2d15e93adf4fc17d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-09-10 10:46:21 +0300

    .

---
+++ WhiteStone.Test/LanguageTranslations/TedTalksSubtitle/NormalizerTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.LanguageTranslations.TedTalksSubtitle
+{
+    [TestClass]
+    public  class NormalizerTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            const string data = @"
+
+1
+00:00:12,174 --> 00:00:15,306
+Chris Anderson: Elon, nasıl çılgın bir hayal
+Chris Anderson: Elon, what kind of crazy dream
+
+2
+00:00:15,306 --> 00:00:17,462
+seni otomobil sanayisine girmeye ve
+would persuade you to think of trying
+
+3
+00:00:17,462 --> 00:00:20,820
+elektrikli araba üretmeyi düşünmeye ikna etti?
+to take on the auto industry and build an all-electric car?
+
+4
+00:00:20,820 --> 00:00:24,222
+Elon Musk: Aslında hikaye üniversite dönemime kadar uzanıyor.
+Elon Musk: Well, it goes back to when I was in university.
+
+
+";
+            var result = Normalizer.Normalize(data);
+
+            const string expectedResult = @"
+Chris Anderson: Elon, nasıl çılgın bir hayal seni otomobil sanayisine girmeye ve elektrikli araba üretmeyi düşünmeye ikna etti?
+Chris Anderson: Elon, what kind of crazy dream would persuade you to think of trying to take on the auto industry and build an all-electric car?
+
+Elon Musk: Aslında hikaye üniversite dönemime kadar uzanıyor.
+Elon Musk: Well, it goes back to when I was in university.
+
+";
+
+            Assert.AreEqual(expectedResult.Trim(),result.Trim());
+
+        }
+    }
+}
---
+++ WhiteStone/LanguageTranslations/TedTalksSubtitle/Normalizer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BOA.LanguageTranslations.TedTalksSubtitle
+{
+    /// <summary>
+    ///     The normalizer
+    /// </summary>
+    public static class Normalizer
+    {
+        #region Public Methods
+        /// <summary>
+        ///     Normalizes the specified content.
+        /// </summary>
+        public static string Normalize(string content)
+        {
+            var lines = new List<LineInfo>();
+
+            string line       = null;
+            string nextLine   = null;
+            var    lineNumber = 0;
+
+            var strings = content.Split(Environment.NewLine.ToCharArray()).ToArray();
+            var length  = strings.Length;
+            for (var i = 0; i < length; i++)
+            {
+                line = strings[i];
+
+                nextLine = null;
+                if (i + 1 < length)
+                {
+                    nextLine = strings[i + 1];
+                }
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                if (int.TryParse(line, out lineNumber))
+                {
+                    if (nextLine?.Contains(" --> ") == true)
+                    {
+                        lines.Add(new LineInfo
+                        {
+                            Tr = strings[i + 2],
+                            En = strings[i + 3]
+                        });
+                        i = i + 3;
+                    }
+                }
+            }
+
+            // start to match line for sentences
+
+            lines = CombineForSentences(lines);
+
+            var sb = new StringBuilder();
+            foreach (var lineInfo in lines)
+            {
+                sb.AppendLine("");
+                sb.AppendLine(lineInfo.Tr.Trim());
+                sb.AppendLine(lineInfo.En.Trim());
+            }
+
+            return sb.ToString().Trim();
+        }
+        #endregion
+
+        #region Methods
+        static List<LineInfo> CombineForSentences(List<LineInfo> lines)
+        {
+            var sentences = new List<LineInfo>();
+
+            var index = 0;
+
+            for (var i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                if (line.Tr.Trim().EndsWith(".") || line.Tr.Trim().EndsWith("?"))
+                {
+                    var tr = "";
+                    var en = "";
+                    for (; index <= i; index++)
+                    {
+                        tr += " " + lines[index].Tr;
+                        en += " " + lines[index].En;
+                    }
+
+                    sentences.Add(new LineInfo
+                    {
+                        Tr = tr,
+                        En = en
+                    });
+                }
+            }
+
+            return sentences;
+        }
+        #endregion
+
+        class LineInfo
+        {
+            #region Public Properties
+            public string En { get; set; }
+            public string Tr { get; set; }
+            #endregion
+        }
+    }
+}
