8960e1eef12303cbc242d2bb72a9e7b4da1a11a9
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-11-23 10:48:24 +0300

    .

--- WhiteStone/LanguageTranslations/EnglishBank/Parser.cs
+++ WhiteStone/LanguageTranslations/EnglishBank/Parser.cs
@@ -3,19 +3,63 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using BOA.Common.Helpers;
-using WhiteStone.Services;
 
 namespace BOA.LanguageTranslations.EnglishBank
 {
+    /// <summary>
+    ///     The word information
+    /// </summary>
+    [Serializable]
     public class WordInfo
     {
-        public string RawContent { get; set; }
+        #region Public Properties
+        /// <summary>
+        ///     Gets or sets the details.
+        /// </summary>
+        public IReadOnlyCollection<string> Details { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the key.
+        /// </summary>
+        public string Key { get; set; }
+        #endregion
+
+        #region Properties
+        internal string RawContent { get; set; }
+        #endregion
+
+        #region Methods
+        internal void Parse()
+        {
+            var arr = RawContent.Split(";(".ToCharArray());
+            Key = arr[0].Trim();
+
+            var lines = new List<string>();
+            var i     = 0;
+            foreach (var line in RawContent.Split(Environment.NewLine.ToCharArray()).Where(x => string.IsNullOrWhiteSpace(x) == false).Select(x=>x.Trim()))
+            {
+                if (i++ == 0 && line.StartsWith(Key))
+                {
+                    if (!string.IsNullOrWhiteSpace(line.RemoveFromStart(Key)))
+                    {
+                        lines.Add(line.RemoveFromStart(Key).RemoveFromStart(";").Trim());
+                    }
+
+                    continue;
+                }
+
+                lines.Add(line.Trim().RemoveFromStart(";").Trim());
+            }
+
+            Details = lines;
+        }
+        #endregion
     }
 
     class Parser
     {
         #region Static Fields
-        static readonly int[] AbsentIndex = {251,788,2973, 2974, 2985, 2986, 2988, 2989, 2990 };
+        static readonly int[] AbsentIndex = {251, 788, 2973, 2974, 2985, 2986, 2988, 2989, 2990};
         #endregion
 
         #region Public Methods
@@ -50,13 +94,13 @@ namespace BOA.LanguageTranslations.EnglishBank
                     RawContent = allText.Substring(lastIndex, index - lastIndex)
                 };
 
+                wordInfo.Parse();
+
                 words.Add(wordInfo);
 
-                File.WriteAllText( "d:\\temp\\"+i+".txt", JsonHelper.Serialize(wordInfo));
+                File.WriteAllText($"d:\\temp\\{i +" - "+ wordInfo.Key}.json", JsonHelper.Serialize(wordInfo));
 
-                lastIndex = index+ number.Length;
-
-
+                lastIndex = index + number.Length;
             }
 
             return words;
