a7196ccbe303bfc59a051652ba91c3a027685b81
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-12-01 00:45:08 +0300

    .

--- BOA.HelperTools/BOAPlugins/FormApplicationGenerator/OrchestrationFileForListForm.cs
+++ BOA.HelperTools/BOAPlugins/FormApplicationGenerator/OrchestrationFileForListForm.cs
@@ -1,73 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace BOAPlugins.FormApplicationGenerator
 {
+    partial class OrchestrationFile
+    {
+        #region Public Properties
+        public string                ClassName                   { get; set; }
+        public string                DefinitionFormDataClassName { get; set; }
+        public IReadOnlyList<string> GridColumnFields            { get; set; }
+        public string                NamespaceName               { get; set; }
+        public string                NamespaceNameForType        { get; set; }
+        public string                RequestName                 { get; set; }
+        #endregion
+    }
+
     static class OrchestrationFileForListForm
     {
         #region Public Methods
         public static string GenerateCode(Model Model)
         {
-            var resultGridColumnNames = string.Join("," + Environment.NewLine, Model.FormDataClassFields.Select(fieldInfo => $"nameof({Model.DefinitionFormDataClassName}.{fieldInfo.Name})"));
-
-            return @"
-using System.Collections.Generic;
-using BOA.Base;
-using BOA.Common.Types;
-using BOA.Common.Helpers;
-using " + Model.NamespaceNameForType + @";
-
-namespace " + Model.NamespaceNameForOrchestration + @"
-{   
-    public class " + Model.FormName + @"ListForm
-    {
-        /// <summary>
-        ///     Loads the data.
-        /// </summary>
-        public GenericResponse<" + Model.RequestNameForList + @"> LoadData(" + Model.RequestNameForList + @" request, ObjectHelper objectHelper)
-        {
-            var returnObject = objectHelper.InitializeResponse(request);
-            var dataSource   = request.DataSource;
-
-
-            dataSource.DataGridInfo = GetDataGridInfo();
-            dataSource.Records      = new List<" + Model.DefinitionFormDataClassName + @">();
-
-            return returnObject;
-        }
-
-        /// <summary>
-        ///     Gets the information.
-        /// </summary>
-        public GenericResponse<" + Model.RequestNameForList + @"> GetInfo(" + Model.RequestNameForList + @" request, ObjectHelper objectHelper)
-        {
-            var returnObject = objectHelper.InitializeResponse(request);
-            var data         = request.Data;
-            var dataSource   = request.DataSource;
-           
-
-            #region TODO: Expects code
-            dataSource.Records = RandomValue.ListOf<" + Model.DefinitionFormDataClassName + @">();
-            #endregion
-
-            request.StatusMessage = dataSource.Records.Count + Message.RecordsWereBrought;
-
-            return returnObject;
-        }
-
-        /// <summary>
-        ///     Gets the data grid information.
-        /// </summary>
-        static DataGridInfo GetDataGridInfo()
-        {
-            return DataGridInfo.Create(typeof(" + Model.DefinitionFormDataClassName + @"), new[]
+            var orchestrationFileTemplate = new OrchestrationFile
             {
-                " + resultGridColumnNames + @"
-            });
-        }        
-    }
-}
-";
+                NamespaceNameForType        = Model.NamespaceNameForType,
+                NamespaceName               = Model.NamespaceNameForOrchestration,
+                ClassName                   = Model.FormName + "ListForm",
+                RequestName                 = Model.RequestNameForList,
+                DefinitionFormDataClassName = Model.DefinitionFormDataClassName,
+                GridColumnFields            = Model.FormDataClassFields.Select(fieldInfo => fieldInfo.Name).ToArray()
+            };
+            return orchestrationFileTemplate.TransformText();
         }
         #endregion
     }
---
+++ BOA.HelperTools/Tests/BOAPlugins.Test/FormApplicationGenerator/Test1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOA.Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOAPlugins.FormApplicationGenerator
+{
+    [TestClass]
+    public  class Test1
+    {
+        [TestMethod]
+        public void A()
+        {
+            var a = new OrchestrationFile
+            {
+                GridColumnFields = new []{"A","B"},
+                NamespaceNameForType = "f",
+                ClassName = "u",
+                DefinitionFormDataClassName = "yy",
+                NamespaceName = "hh",
+                RequestName = "Req"
+            }.TransformText();
+            a.ToString();
+
+        }
+    }
+}
