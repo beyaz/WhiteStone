d4e66259f2cfb8d0f9294d5de999d7750b3343ab
Abdullah Beyaztaş <beyaz1404@gmail.com>
2018-12-05 00:03:49 +0300

    .

--- BOA.HelperTools/BOAPlugins/FormApplicationGenerator/Types/BField.cs
+++ BOA.HelperTools/BOAPlugins/FormApplicationGenerator/Types/BField.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BOA.Common.Helpers;
 
 namespace BOAPlugins.FormApplicationGenerator.Types
 {
@@ -6,19 +7,26 @@ namespace BOAPlugins.FormApplicationGenerator.Types
     public abstract class BField
     {
         #region Public Properties
-        public string BindingPath { get;  set; }
-        public string Label { get; set; }
-        public string SnapName { get; set; }
+        public string BindingPath { get; set; }
+        public string Label       { get; set; }
+
+        public string SnapName
+        {
+            get
+            {
+                var typeName = GetType().Name.RemoveFromStart("B");
+
+                return typeName[0].ToString().ToLowerTR() + typeName.Substring(1) + BindingPath;
+            }
+        }
         #endregion
     }
-
-   
 
     [Serializable]
     public class BInputMask : BField
     {
         #region Public Properties
-        public bool   IsCreditCard { get; set; }
+        public bool IsCreditCard { get; set; }
         #endregion
     }
 }
