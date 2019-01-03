09129807d9d3d9667ba745f63761073efcdaffa5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-04 00:23:46 +0300

    .

---
+++ WhiteStone.Test/Services/JsonFileTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.Services
+{
+    [TestClass]
+    public class JsonFileTest
+    {
+        #region Public Properties
+        public string Prop1 { get; set; }
+        #endregion
+
+        [TestMethod]
+        public void Should_throw_exception_when_file_path_is_null()
+        {
+            Func<JsonFile<JsonFileTest>> action = () => new JsonFile<JsonFileTest>(null);
+
+            action.Should().Throw<ArgumentNullException>();
+
+        }
+        #region Public Methods
+        [TestMethod]
+        public void Should_save_to_file_if_file_not_exists()
+        {
+            var jsonFile = new JsonFile<JsonFileTest>("A\\t.txt");
+
+            jsonFile.Delete();
+
+            var data  = jsonFile.Load();
+
+            data.Prop1 = "A";
+
+            // ACT
+            jsonFile.Save(data);
+
+            data = jsonFile.Load();
+
+            // ASSERT
+            data.Prop1.Should().Be("A");
+        }
+
+        
+
+        #endregion
+    }
+}
---
+++ WhiteStone/Services/JsonFile.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using BOA.Common.Helpers;
+
+namespace BOA.Services
+{
+    /// <summary>
+    ///     The json file
+    /// </summary>
+    public class JsonFile<T> where T : new()
+    {
+        #region Constructors
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="JsonFile{T}" /> class.
+        /// </summary>
+        public JsonFile(string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            FilePath = filePath;
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        ///     Gets the file path.
+        /// </summary>
+        public string FilePath { get; }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        ///     Deletes this instance.
+        /// </summary>
+        public void Delete()
+        {
+            if (File.Exists(FilePath))
+            {
+                File.Delete(FilePath);
+            }
+        }
+
+        /// <summary>
+        ///     Loads this instance.
+        /// </summary>
+        public T Load()
+        {
+            var fileNotExists = File.Exists(FilePath) == false;
+            if (fileNotExists)
+            {
+                Save(new T());
+            }
+
+            var configurationAsString = File.ReadAllText(FilePath);
+
+            return JsonHelper.Deserialize<T>(configurationAsString);
+        }
+
+        /// <summary>
+        ///     Saves the specified configuration.
+        /// </summary>
+        public void Save(T configuration)
+        {
+            FileHelper.WriteAllText(FilePath, JsonHelper.Serialize(configuration));
+        }
+        #endregion
+    }
+}
