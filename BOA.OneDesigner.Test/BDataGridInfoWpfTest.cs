7777f19549e0f9942894104e81306f0854fd3165
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-05 18:08:17 +0300

    .

---
+++ BOA.OneDesigner.Test/BDataGridInfoWpfTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using BOA.OneDesigner.AppModel;
+using BOA.OneDesigner.JsxElementModel;
+using BOA.OneDesigner.WpfControls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.OneDesigner.WpfControls
+{
+    [TestClass]
+    public class BDataGridInfoWpfTest
+    {
+        [TestMethod]
+        public void Should_be_zero_column_when_in_toolbox()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Can_drop_only_own_columns()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Drop_locations_should_be_un_visible_when_in_drop_location_mode_is_true()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Drop_locations_should_be_un_visible_when_in_drop_location_mode_is_false()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Drop_locations_should_be_un_visible_when_any_own_column_removed()
+        {
+            
+        }
+    }
+}
