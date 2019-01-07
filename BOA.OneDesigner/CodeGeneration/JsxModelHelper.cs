170d192d6dac52fceab4b26a543cbd0cca07b1ce
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-07 11:25:54 +0300

    .

---
+++ BOA.OneDesigner/CodeGeneration/JsxModelHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BOA.Common.Helpers;
+using BOA.OneDesigner.AppModel;
+using BOA.OneDesigner.JsxElementModel;
+
+namespace BOA.OneDesigner.CodeGeneration
+{
+    static class JsxModelHelper
+    {
+        #region Public Methods
+        public static bool HasComponent<TComponentType>(this DivAsCardContainer divAsCardContainer)
+        {
+            var items = new List<object>();
+
+            divAsCardContainer.VisitAllBComponents(c =>
+            {
+                if (c.GetType() == typeof(TComponentType))
+                {
+                    items.Add(c);
+                }
+            });
+
+            return items.Any();
+        }
+
+        public static void VisitAllBComponents(this DivAsCardContainer divAsCardContainer, Action<object> action)
+        {
+            foreach (var card in divAsCardContainer.Items)
+            {
+                foreach (var item in card.Items)
+                {
+                    action(item);
+                }
+
+                action(card);
+            }
+        }
+
+        public static void Write(this PaddedStringBuilder sb, DivAsCardContainer data)
+        {
+            sb.AppendLine("<div>");
+
+            sb.PaddingCount++;
+
+            foreach (var bCard in data.Items)
+            {
+                sb.Write(bCard);
+
+                sb.AppendLine(string.Empty);
+            }
+
+            sb.PaddingCount--;
+
+            sb.AppendLine("</div>");
+        }
+
+        public static void Write(this PaddedStringBuilder sb, BCard data)
+        {
+            sb.Append("<BCard context={context}");
+            var hasTitle = string.IsNullOrWhiteSpace(data.Title) == false;
+            if (hasTitle)
+            {
+                sb.AppendLine($" title = {{{data.Title}}}");
+            }
+
+            sb.AppendLine(">");
+
+            sb.PaddingCount++;
+
+            foreach (var item in data.Items)
+            {
+                var bInput = item as BInput;
+                if (bInput != null)
+                {
+                    sb.Write(bInput);
+                    continue;
+                }
+
+                throw Error.InvalidOperation();
+            }
+
+            sb.PaddingCount--;
+            sb.Append("</BCard>");
+        }
+
+        public static void Write(this PaddedStringBuilder sb, BInput data)
+        {
+            sb.AppendLine($"<BInput value = {{request.{data.BindingPath}}}");
+            sb.PaddingCount += 2;
+
+            sb.AppendLine($"onChange = {{(e: any, value: string) => {data.BindingPath} = value}}");
+            sb.AppendLine($"floatingLabelTextDate = {{{data.Label}}}");
+            sb.AppendLine("context = {context}/>");
+
+            sb.PaddingCount -= 2;
+        }
+        #endregion
+    }
+}
---
+++ BOA.OneDesigner/CodeGeneration/TransactionPage.cs
@@ -0,0 +1,124 @@
+﻿using System.Linq;
+using BOA.Common.Helpers;
+using BOA.OneDesigner.JsxElementModel;
+
+namespace BOA.OneDesigner.CodeGeneration
+{
+    class TransactionPage
+    {
+        #region Public Methods
+        public string Generate(ScreenInfo screenInfo)
+        {
+            var sb       = new PaddedStringBuilder();
+            var jsxModel = (DivAsCardContainer) screenInfo.JsxModel;
+
+            // GeneralParametersFormRequest
+            var className = screenInfo.RequestName.SplitAndClear(".").Last().RemoveFromEnd("Request");
+
+            sb.AppendLine("import * as React from \"react\"");
+            sb.AppendLine("import { TransactionPage, TransactionPageComposer } from \"b-framework\"");
+            sb.AppendLine("import { BCard } from \"b-card\"");
+
+            if (jsxModel.HasComponent<BInput>())
+            {
+                sb.AppendLine("import { BInput } from \"b-input\"");
+            }
+
+            sb.AppendLine(string.Empty);
+            sb.AppendLine($"class {className} extends TransactionPage");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.AppendLine(string.Empty);
+
+            #region constructor
+            sb.Append("constructor(props: BFramework.BasePageProps)");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.Append("super(props);");
+            sb.Append("this.connect(this);");
+            sb.Append("FormAssistant.initialize(this, RequestName.GeneralParametersFormRequest);");
+
+            sb.PaddingCount--;
+            sb.Append("}");
+            #endregion
+
+            sb.AppendLine(string.Empty);
+
+            #region onActionClick
+            sb.Append("onActionClick(command: BOA.Common.Types.ResourceActionContract)");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.Append("FormAssistant.executeWindowRequest(this,command.commandName);");
+            sb.Append("return /*isCompleted*/true;");
+
+            sb.PaddingCount--;
+            sb.Append("}");
+            #endregion
+
+            sb.AppendLine(string.Empty);
+
+            #region componentDidMount
+            sb.Append("componentDidMount()");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.Append("super.componentDidMount();");
+            sb.Append("FormAssistant.componentDidMount(this);");
+
+            sb.PaddingCount--;
+            sb.Append("}");
+            #endregion
+
+            sb.AppendLine(string.Empty);
+
+            #region proxyDidRespond
+            sb.Append("proxyDidRespond(proxyResponse: ProxyResponse)");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.Append("return FormAssistant.receiveResponse(this,proxyResponse);");
+
+            sb.PaddingCount--;
+            sb.Append("}");
+            #endregion
+
+            sb.AppendLine(string.Empty);
+
+            #region render
+            sb.Append("render()");
+            sb.Append("{");
+            sb.PaddingCount++;
+
+            sb.Append("if (!FormAssistant.isReadyToRender(this))");
+            sb.Append("{");
+            sb.PaddingCount++;
+            sb.Append("return <div/>;");
+            sb.PaddingCount--;
+            sb.Append("}");
+
+            sb.AppendLine(string.Empty);
+            sb.Append("const context = this.state.context;");
+
+            sb.Append("const windowRequest: GeneralParametersFormRequest = FormAssistant.getWindowRequest(this);");
+
+            sb.Append("return (");
+            sb.PaddingCount++;
+            sb.Write(jsxModel);
+            sb.PaddingCount--;
+            sb.Append(")");
+
+            sb.PaddingCount--;
+            sb.Append("}");
+            #endregion
+
+            sb.PaddingCount--;
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
