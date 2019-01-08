99f2462550d0ee2566e15b7190e1a3dad46bed1d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-08 10:27:09 +0300

    .

--- BOA.OneDesigner/CodeGeneration/JsxModelHelper.cs
+++ BOA.OneDesigner/CodeGeneration/JsxModelHelper.cs
@@ -49,7 +49,7 @@ namespace BOA.OneDesigner.CodeGeneration
 
             foreach (var bCard in data.Items)
             {
-                sb.Write(screenInfo,bCard);
+                sb.Write(screenInfo, bCard);
 
                 sb.AppendLine(string.Empty);
             }
@@ -59,66 +59,33 @@ namespace BOA.OneDesigner.CodeGeneration
             sb.AppendLine("</div>");
         }
 
-
-        static string GetLabelValue(ScreenInfo screenInfo, LabelInfo data)
-        {
-            if (data == null)
-            {
-                return null;
-            }
-
-            if (data.IsFreeText)
-            {
-
-                return '"' + data.FreeTextValue + '"';
-            }
-
-            if(data.IsRequestBindingPath)
-            {
-                return NormalizeBindingPath("request." + data.RequestBindingPath);
-            }
-            if(data.IsFromMessaging)
-            {
-                return $"getMessage(\"{screenInfo.MessagingGroupName}\", \"{data.MessagingValue}\")";
-            }
-
-            throw Error.InvalidOperation();
-        }
-
-        static string NormalizeBindingPath(string propertyNameInCSharp)
-        {
-
-            return string.Join(".", propertyNameInCSharp.SplitAndClear(".").ToList().ConvertAll(TypescriptNaming.GetResolvedPropertyName));
-        }
-
-
         public static void Write(this PaddedStringBuilder sb, ScreenInfo screenInfo, BCard data)
         {
             sb.AppendWithPadding("<BCard context={context}");
 
-            var labelValue = GetLabelValue(screenInfo,data.TitleInfo);
+            var labelValue = GetLabelValue(screenInfo, data.TitleInfo);
             if (labelValue != null)
             {
-                sb.Append($" title = {{{labelValue}}}");  
+                sb.Append($" title = {{{labelValue}}}");
             }
 
             if (data.SizeInfo?.IsLarge == true)
             {
-                sb.Append(" layoutProps = {{w:12}}");  
+                sb.Append(" layoutProps = {{w:12}}");
             }
             else if (data.SizeInfo?.IsMedium == true)
             {
-                sb.Append(" layoutProps = {{w:6}}");  
+                sb.Append(" layoutProps = {{w:6}}");
             }
             else if (data.SizeInfo?.IsSmall == true)
             {
-                sb.Append(" layoutProps = {{w:4}}");  
+                sb.Append(" layoutProps = {{w:4}}");
             }
             else if (data.SizeInfo?.IsExtraSmall == true)
             {
-                sb.Append(" layoutProps = {{w:3}}");  
+                sb.Append(" layoutProps = {{w:3}}");
             }
-            
+
             sb.Append(">");
             sb.Append(Environment.NewLine);
 
@@ -131,7 +98,7 @@ namespace BOA.OneDesigner.CodeGeneration
                 var bInput = item as BInput;
                 if (bInput != null)
                 {
-                    sb.Write(screenInfo,bInput);
+                    sb.Write(screenInfo, bInput);
                     continue;
                 }
 
@@ -142,16 +109,14 @@ namespace BOA.OneDesigner.CodeGeneration
             sb.AppendLine("</BCard>");
         }
 
-        public static void Write(this PaddedStringBuilder sb, ScreenInfo screenInfo ,BInput data)
+        public static void Write(this PaddedStringBuilder sb, ScreenInfo screenInfo, BInput data)
         {
-            
             var solutionInfo = SolutionInfo.CreateFromTfsFolderPath(screenInfo.TfsFolderName);
 
-            var propertyDefinition = CecilHelper.FindPropertyInfo(solutionInfo.TypeAssemblyPathInServerBin,screenInfo.RequestName,data.BindingPath);
+            var propertyDefinition = CecilHelper.FindPropertyInfo(solutionInfo.TypeAssemblyPathInServerBin, screenInfo.RequestName, data.BindingPath);
 
             var isDecimal = CecilHelper.FullNameOfNullableDecimal == propertyDefinition.PropertyType.FullName ||
                             propertyDefinition.PropertyType.FullName == typeof(decimal).FullName;
-
 
             var isString = propertyDefinition.PropertyType.FullName == typeof(string).FullName;
 
@@ -159,14 +124,12 @@ namespace BOA.OneDesigner.CodeGeneration
 
             if (isString)
             {
-
                 sb.AppendLine($"<BInput value = {{{bindingPathInJs}}}");
                 sb.PaddingCount++;
 
                 sb.AppendLine($"onChange = {{(e: any, value: string) => {bindingPathInJs} = value}}");
-
             }
-            else if(isDecimal)
+            else if (isDecimal)
             {
                 sb.AppendLine($"<BInputNumeric value = {{{bindingPathInJs}}}");
                 sb.PaddingCount++;
@@ -183,37 +146,75 @@ namespace BOA.OneDesigner.CodeGeneration
                 sb.AppendLine($"onChange = {{(e: any, value: number) => {bindingPathInJs} = value}}");
                 sb.AppendLine("maxLength = {10}");
             }
-            
 
-
-            var labelValue = GetLabelValue(screenInfo,data.LabelInfo);
+            var labelValue = GetLabelValue(screenInfo, data.LabelInfo);
             if (labelValue != null)
             {
-                sb.AppendLine($"floatingLabelText = {labelValue}");  
+                sb.AppendLine($"floatingLabelText = {labelValue}");
             }
 
+            if (!string.IsNullOrWhiteSpace(data.IsVisibleBindingPath))
+            {
+                sb.AppendLine($"isVisible = {{{NormalizeBindingPath("request." + data.IsVisibleBindingPath)}}}");
+            }
 
-            
+            if (!string.IsNullOrWhiteSpace(data.IsDisabledBindingPath))
+            {
+                sb.AppendLine($"disabled = {{{NormalizeBindingPath("request." + data.IsDisabledBindingPath)}}}");
+            }
+
             if (data.SizeInfo?.IsLarge == true)
             {
-                sb.AppendLine("size = {ComponentSize.LARGE}");  
+                sb.AppendLine("size = {ComponentSize.LARGE}");
             }
             else if (data.SizeInfo?.IsMedium == true)
             {
-                sb.AppendLine("size = {ComponentSize.MEDIUM}");  
+                sb.AppendLine("size = {ComponentSize.MEDIUM}");
             }
             else if (data.SizeInfo?.IsSmall == true)
             {
-                sb.AppendLine("size = {ComponentSize.SMALL}");   
+                sb.AppendLine("size = {ComponentSize.SMALL}");
             }
             else if (data.SizeInfo?.IsExtraSmall == true)
             {
-                sb.AppendLine("size = {ComponentSize.XSMALL}");   
+                sb.AppendLine("size = {ComponentSize.XSMALL}");
             }
 
             sb.AppendLine("context = {context}/>");
 
             sb.PaddingCount--;
+        }
+        #endregion
+
+        #region Methods
+        static string GetLabelValue(ScreenInfo screenInfo, LabelInfo data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+
+            if (data.IsFreeText)
+            {
+                return '"' + data.FreeTextValue + '"';
+            }
+
+            if (data.IsRequestBindingPath)
+            {
+                return NormalizeBindingPath("request." + data.RequestBindingPath);
+            }
+
+            if (data.IsFromMessaging)
+            {
+                return $"getMessage(\"{screenInfo.MessagingGroupName}\", \"{data.MessagingValue}\")";
+            }
+
+            throw Error.InvalidOperation();
+        }
+
+        static string NormalizeBindingPath(string propertyNameInCSharp)
+        {
+            return string.Join(".", propertyNameInCSharp.SplitAndClear(".").ToList().ConvertAll(TypescriptNaming.GetResolvedPropertyName));
         }
         #endregion
     }
