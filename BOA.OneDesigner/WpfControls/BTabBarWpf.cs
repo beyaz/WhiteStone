4355a09067fdbce242784c832cc947d6e57a4f8d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-10 20:44:46 +0300

    .

--- BOA.OneDesigner/WpfControls/BTabBarWpf.cs
+++ BOA.OneDesigner/WpfControls/BTabBarWpf.cs
@@ -66,7 +66,7 @@ namespace BOA.OneDesigner.WpfControls
 
             if (Model == null)
             {
-                return;
+                throw Error.InvalidOperation();
             }
 
             foreach (var bTabBarPage in Model.Items)
