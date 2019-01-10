2207dd34510c41658d6c65398149256c90582a99
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-01-10 20:40:21 +0300

    .

--- BOA.OneDesigner/WpfControls/BTabBarWpf.cs
+++ BOA.OneDesigner/WpfControls/BTabBarWpf.cs
@@ -22,8 +22,6 @@ namespace BOA.OneDesigner.WpfControls
         #region Constructors
         public BTabBarWpf()
         {
-            
-
             var stackPanel = new StackPanel();
             stackPanel.Children.Add(HeadersContainersWrapPanel);
             stackPanel.Children.Add(TabPageBodyList);
@@ -168,7 +166,6 @@ namespace BOA.OneDesigner.WpfControls
 
             OnAttachToEventBus?.Invoke();
 
-            //Host.EventBus.Subscribe(EventBus.OnComponentPropertyChanged, Refresh);
             Host.EventBus.Subscribe(EventBus.TabBarPageRemoved, OnTabPageRemoved);
         }
 
@@ -180,7 +177,6 @@ namespace BOA.OneDesigner.WpfControls
             }
 
             OnDeAttachToEventBus?.Invoke();
-            //Host.EventBus.UnSubscribe(EventBus.OnComponentPropertyChanged, Refresh);
             Host.EventBus.UnSubscribe(EventBus.TabBarPageRemoved, OnTabPageRemoved);
         }
         #endregion
