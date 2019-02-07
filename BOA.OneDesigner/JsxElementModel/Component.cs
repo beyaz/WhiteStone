8e4cc30155312f11989881a3e44730264f49b133
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-02-07 14:22:33 +0300

    .

---
+++ BOA.OneDesigner/JsxElementModel/Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BOA.OneDesigner.JsxElementModel
+{
+    [Serializable]
+    public class Component : BField
+    {
+        #region Public Properties
+        public ComponentType Type { get; set; }
+        #endregion
+    }
+}
---
+++ BOA.OneDesigner/JsxElementModel/ComponentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BOA.OneDesigner.JsxElementModel
+{
+    [Serializable]
+    public class ComponentType
+    {
+        #region Public Properties
+        public bool IsDivider { get; set; }
+        #endregion
+    }
+}
