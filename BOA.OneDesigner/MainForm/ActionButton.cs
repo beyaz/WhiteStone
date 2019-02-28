56a55a99910a43cd9e3339c0ef23d61a3fcdbb09
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-02-28 17:01:54 +0300

    .

---
+++ BOA.OneDesigner/MainForm/ActionButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BOA.OneDesigner.AppModel;
+
+namespace BOA.OneDesigner.MainForm
+{
+    class ActionButton : Button
+    {
+        #region Constructors
+        public ActionButton(Aut_ResourceAction action)
+        {
+            Model = action;
+
+            Content = action.Name;
+
+            Padding = new Thickness(5);
+
+            Margin = new Thickness(5);
+        }
+        #endregion
+
+        #region Public Properties
+        public Aut_ResourceAction Model { get; }
+        #endregion
+    }
+}
--- BOA.OneDesigner/MainForm/View.cs
+++ BOA.OneDesigner/MainForm/View.cs
@@ -123,24 +123,4 @@ namespace BOA.OneDesigner.MainForm
         }
         #endregion
     }
-
-    class ActionButton : Button
-    {
-        #region Constructors
-        public ActionButton(Aut_ResourceAction action)
-        {
-            Model = action;
-
-            Content = action.Name;
-
-            Padding = new Thickness(5);
-
-            Margin = new Thickness(5);
-        }
-        #endregion
-
-        #region Public Properties
-        public Aut_ResourceAction Model { get; }
-        #endregion
-    }
 }
