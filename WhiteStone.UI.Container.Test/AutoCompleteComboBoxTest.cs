6f64315a6e30c9078e0392ab8afd0617bde35303
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-03-19 18:58:00 +0300

    .

---
+++ WhiteStone.UI.Container.Test/AutoCompleteComboBoxTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using DotNetKit.Windows.Controls;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WhiteStone.UI.Container.Test
+{
+    public class UserInfo
+    {
+        #region Public Properties
+        public string Id   { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region Public Methods
+        public override string ToString()
+        {
+            return Id + " - " + Name;
+        }
+        #endregion
+    }
+
+    [TestClass]
+    public class AutoCompleteComboBoxTest
+    {
+        #region Fields
+        List<UserInfo> Users;
+        #endregion
+
+        #region Public Methods
+        [TestInitialize]
+        public void Initialize()
+        {
+            Users = new List<UserInfo>
+            {
+                new UserInfo {Name = "Name0", Id = "0"},
+                new UserInfo {Name = "Name1", Id = "1"},
+                new UserInfo {Name = "Name2", Id = "2"}
+            };
+        }
+
+
+        [TestMethod]
+        public void Test1()
+        {
+
+
+            var autoCompleteComboBox = new AutoCompleteComboBox
+            {
+                DisplayMemberPath = nameof(UserInfo.Name),
+                SelectedValuePath = nameof(UserInfo.Id),
+                ItemsSource       = Users
+            };
+
+            autoCompleteComboBox.ApplyTemplate();
+
+
+            // autoCompleteComboBox.RaiseEvent(new RoutedEventArgs(FrameworkElement.LoadedEvent));
+
+            autoCompleteComboBox.SelectedValue = "1";
+            autoCompleteComboBox.SelectedItem.Should().BeSameAs(Users[1]);
+            autoCompleteComboBox.Text.Should().BeSameAs("Name1");
+
+            autoCompleteComboBox.EditableTextBox.Text.Should().BeSameAs("Name1");
+
+
+            autoCompleteComboBox.SelectedValue = "2";
+            autoCompleteComboBox.SelectedItem.Should().BeSameAs(Users[2]);
+            autoCompleteComboBox.Text.Should().BeSameAs("Name2");
+            autoCompleteComboBox.EditableTextBox.Text.Should().BeSameAs("Name2");
+
+           
+        }
+        #endregion
+    }
+}
