7a1fca07124ef864ab0097047b49f3e1608559b3
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-03-22 12:34:16 +0300

    .

--- BOACardCustomSqlHelper/MainForm/Model.cs
+++ BOACardCustomSqlHelper/MainForm/Model.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using BOA.OneDesigner.JsxElementModel;
-using BOAPlugins.TypescriptModelGeneration;
 using WhiteStone.UI.Container.Mvc;
 
 namespace BOACardCustomSqlHelper.MainForm
@@ -13,44 +11,7 @@ namespace BOACardCustomSqlHelper.MainForm
     public class Model : ModelBase
     {
         #region Public Properties
-        /// <summary>
-        ///     Gets or sets a value indicating whether [design tab is visible].
-        /// </summary>
-        public bool DesignIsVisible { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the form types.
-        /// </summary>
-        public IReadOnlyList<string> FormTypes { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the messaging group names.
-        /// </summary>
-        public IReadOnlyList<string> MessagingGroupNames { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the request names.
-        /// </summary>
-        public IReadOnlyList<string> RequestNames { get; set; }
-
-        public ScreenInfo ScreenInfo { get; set; }
-
-        public bool SearchIsVisible { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the solution file path.
-        /// </summary>
-        public string SolutionFilePath { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the solution information.
-        /// </summary>
-        public SolutionInfo SolutionInfo { get; set; }
-
-        /// <summary>
-        ///     Gets or sets the TFS folder names.
-        /// </summary>
-        public IReadOnlyList<string> TfsFolderNames { get; set; }
+        public IReadOnlyCollection<string> ProfileIdCollection { get; set; }
         #endregion
     }
 }
---
+++ BOACardCustomSqlHelper/MainForm/UIBuilderHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace BOACardCustomSqlHelper.MainForm
+{
+    public static class UIBuilderHelper
+    {
+        #region Public Methods
+        public static void RegisterElements()
+        {
+        }
+        #endregion
+    }
+}
