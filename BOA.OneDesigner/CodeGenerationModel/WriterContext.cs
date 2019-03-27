83e39ef6f116b103d5feab4ef0876688a55dffc6
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-03-27 12:07:21 +0300

    .

--- BOA.OneDesigner/CodeGenerationModel/WriterContext.cs
+++ BOA.OneDesigner/CodeGenerationModel/WriterContext.cs
@@ -6,116 +6,35 @@ using BOAPlugins.TypescriptModelGeneration;
 
 namespace BOA.OneDesigner.CodeGenerationModel
 {
-    public class TypeScriptMemberInfo
-    {
-
-        
-
-        #region Public Properties
-        public string Code          { get; set; }
-        public bool   IsConstructor { get; set; }
-        public bool   IsField       { get; set; }
-        public bool   IsMethod      { get; set; }
-        public bool   IsRender      { get; set; }
-        #endregion
-
-        #region Public Methods
-        public static int Compare(TypeScriptMemberInfo left, TypeScriptMemberInfo right)
-        {
-            if (left.IsRender && right.IsRender)
-            {
-                return 0;
-            }
-
-            if (left.IsConstructor && right.IsConstructor)
-            {
-                return 0;
-            }
-
-            if (left.IsField && right.IsField)
-            {
-                return 0;
-            }
-
-            if (left.IsMethod && right.IsMethod)
-            {
-                return 0;
-            }
-
-            
-
-            if (left.IsField)
-            {
-                return -1;
-            }
-
-            if ( right.IsField)
-            {
-                return 1;
-            }
-
-            if (left.IsConstructor)
-            {
-                return -1;
-            }
-
-            if (right.IsConstructor)
-            {
-                return 1;
-            }
-
-            if (left.IsMethod)
-            {
-                return -1;
-            }
-
-            return 1;
-        }
-        #endregion
-    }
-
     public class WriterContext
     {
-        
-        public JsObject StateObjectWhenIncomingRequestIsSuccess { get; set; } = new JsObject();
-
-        public List<string> BeforeSetStateOnProxyDidResponse { get; set; }
-
-        public void AddToBeforeSetStateOnProxyDidResponse(string line)
-        {
-            if (BeforeSetStateOnProxyDidResponse == null)
-            {
-                BeforeSetStateOnProxyDidResponse = new List<string>();
-            }
-
-            BeforeSetStateOnProxyDidResponse.Add(line);
-        }
-
-
-        public Dictionary<string,bool> AllNames { get; set; } = new Dictionary<string, bool>();
-
         #region Public Properties
-        public List<TypeScriptMemberInfo> ClassBody { get; set; }
+        public Dictionary<string, bool> AllNames { get; set; } = new Dictionary<string, bool>();
 
-        public string              ClassName       { get; set; }
-        public List<string>        ConstructorBody { get; set; }
-        public bool                HasWorkflow     { get; set; }
-        public List<string>        Imports         { get; set; }
-        public PaddedStringBuilder Output          { get; set; }
+        public List<string>               BeforeSetStateOnProxyDidResponse { get; set; }
+        public bool                       CanWriteEvaluateActions          { get; set; }
+        public List<TypeScriptMemberInfo> ClassBody                        { get; set; }
 
-        public List<string> Page { get; set; }
+        public string                   ClassName                                         { get; set; }
+        public List<string>             ConstructorBody                                   { get; set; }
+        public string                   DataContractAccessPathInWindowRequest             { get; set; }
+        public bool                     DataContractAccessPathInWindowRequestIsCalculated { get; set; }
+        public List<Aut_ResourceAction> EvaluatedActions                                  { get; set; }
+        public bool                     HandleProxyDidRespondCallback                     { get; set; }
+        public bool                     HasWorkflow                                       { get; set; }
+        public List<string>             Imports                                           { get; set; }
+        public bool                     IsBrowsePage                                      { get; set; }
+        public PaddedStringBuilder      Output                                            { get; set; }
+
+        public List<string> Page                                { get; set; }
+        public List<string> RenderMethodRequestRelatedVariables { get; set; } = new List<string>();
 
         public RequestIntellisenseData RequestIntellisenseData { get; set; }
         public ScreenInfo              ScreenInfo              { get; set; }
         public SolutionInfo            SolutionInfo            { get; set; }
-        public List<Aut_ResourceAction> EvaluatedActions { get; set; }
-        public bool CanWriteEvaluateActions { get; set; }
-        public string DataContractAccessPathInWindowRequest { get; set; }
-        public bool DataContractAccessPathInWindowRequestIsCalculated { get; set; }
-        public bool IsBrowsePage { get; set; }
-        public List<string> RenderMethodRequestRelatedVariables { get; set; } = new List<string>();
-        public bool ThrowExceptionOnEmptyActionDefinition { get; set; }
-        public bool HandleProxyDidRespondCallback { get; set; }
+
+        public JsObject StateObjectWhenIncomingRequestIsSuccess { get; set; } = new JsObject();
+        public bool     ThrowExceptionOnEmptyActionDefinition   { get; set; }
         #endregion
 
         #region Public Methods
@@ -127,6 +46,16 @@ namespace BOA.OneDesigner.CodeGenerationModel
         public void AddClassBody(string code)
         {
             ClassBody.Add(new TypeScriptMemberInfo {Code = code, IsMethod = true});
+        }
+
+        public void AddToBeforeSetStateOnProxyDidResponse(string line)
+        {
+            if (BeforeSetStateOnProxyDidResponse == null)
+            {
+                BeforeSetStateOnProxyDidResponse = new List<string>();
+            }
+
+            BeforeSetStateOnProxyDidResponse.Add(line);
         }
         #endregion
     }
