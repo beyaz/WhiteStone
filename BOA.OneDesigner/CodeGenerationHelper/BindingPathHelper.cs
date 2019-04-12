351ad0c196bb812e56700eab97b17341c0b0fd59
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-12 18:07:12 +0300

    .

--- BOA.OneDesigner/CodeGenerationHelper/BindingPathHelper.cs
+++ BOA.OneDesigner/CodeGenerationHelper/BindingPathHelper.cs
@@ -5,9 +5,15 @@ using BOAPlugins.Utility;
 
 namespace BOA.OneDesigner.CodeGenerationHelper
 {
+    /// <summary>
+    ///     The binding path helper
+    /// </summary>
     static class BindingPathHelper
     {
         #region Public Methods
+        /// <summary>
+        ///     Evaluates the window request default creation in render function.
+        /// </summary>
         public static string EvaluateWindowRequestDefaultCreationInRenderFunction(IEnumerable<string> bindingPathsInDesigner)
         {
             var jsObject = new JsObject();
@@ -29,13 +35,22 @@ namespace BOA.OneDesigner.CodeGenerationHelper
         }
         #endregion
 
+        /// <summary>
+        ///     The js object
+        /// </summary>
         class JsObject
         {
             #region Fields
+            /// <summary>
+            ///     The properties
+            /// </summary>
             public readonly List<JsProperty> Properties = new List<JsProperty>();
             #endregion
 
             #region Public Methods
+            /// <summary>
+            ///     Creates the property.
+            /// </summary>
             public JsProperty CreateProperty(string name)
             {
                 var property = new JsProperty {Name = name};
@@ -45,6 +60,9 @@ namespace BOA.OneDesigner.CodeGenerationHelper
                 return property;
             }
 
+            /// <summary>
+            ///     Gets the or create property.
+            /// </summary>
             public JsProperty GetOrCreateProperty(string name)
             {
                 var property = Properties.FirstOrDefault(x => x.Name == name);
@@ -59,11 +77,17 @@ namespace BOA.OneDesigner.CodeGenerationHelper
                 return property;
             }
 
+            /// <summary>
+            ///     Determines whether the specified name has property.
+            /// </summary>
             public bool HasProperty(string name)
             {
                 return Properties.Any(x => x.Name == name);
             }
 
+            /// <summary>
+            ///     Returns a <see cref="System.String" /> that represents this instance.
+            /// </summary>
             public override string ToString()
             {
                 if (Properties.Count == 0)
@@ -76,14 +100,27 @@ namespace BOA.OneDesigner.CodeGenerationHelper
             #endregion
         }
 
+        /// <summary>
+        ///     The js property
+        /// </summary>
         class JsProperty
         {
             #region Public Properties
-            public string Name  { get; set; }
+            /// <summary>
+            ///     Gets or sets the name.
+            /// </summary>
+            public string Name { get; set; }
+
+            /// <summary>
+            ///     Gets or sets the value.
+            /// </summary>
             public object Value { get; set; }
             #endregion
 
             #region Public Methods
+            /// <summary>
+            ///     Returns a <see cref="System.String" /> that represents this instance.
+            /// </summary>
             public override string ToString()
             {
                 if (Value == null)
