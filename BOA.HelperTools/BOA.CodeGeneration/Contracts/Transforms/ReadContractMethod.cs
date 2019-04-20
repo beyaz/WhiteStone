256167bf3d0e72ad46c081245b825e8953e1221f
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-20 14:29:04 +0300

    .

--- BOA.HelperTools/BOA.CodeGeneration/Contracts/Transforms/ReadContractMethod.cs
+++ BOA.HelperTools/BOA.CodeGeneration/Contracts/Transforms/ReadContractMethod.cs
@@ -15,19 +15,15 @@ namespace BOA.CodeGeneration.Contracts.Transforms
         {
             var sb = new PaddedStringBuilder();
 
-            sb.AppendLine($"object {Names.ISupportDmlOperationInfo}.ReadContract(IDataReader reader)");
+            sb.AppendLine($"void {Names.ISupportDmlOperationInfo}.LoadFrom(IDataReader reader)");
             sb.AppendLine("{");
             sb.PaddingCount++;
 
-            sb.AppendLine($"return new {TableInfo.TableName.ToContractName()}Contract");
-            sb.AppendLine("{");
-            sb.PaddingCount++;
 
-            sb.AppendAll(string.Join("," + Environment.NewLine, TableInfo.Columns.Select(GetLine)));
-            sb.AppendLine();
-
-            sb.PaddingCount--;
-            sb.AppendLine("};");
+            foreach (var columnInfo in TableInfo.Columns)
+            {
+                sb.AppendLine(GetLine(columnInfo));
+            }
 
             sb.PaddingCount--;
             sb.AppendLine("}");
@@ -41,10 +37,10 @@ namespace BOA.CodeGeneration.Contracts.Transforms
         {
             if (c.ColumnName == Names.VALID_FLAG)
             {
-                return $"{c.ColumnName.ToContractName()}= DataReaderUtil.{c.SqlReaderMethod}(DataReaderUtil.ReadValue(reader, \"{c.ColumnName}\")) == \"1\"";
+                return $"{c.ColumnName.ToContractName()}= DataReaderUtil.{c.SqlReaderMethod}(DataReaderUtil.ReadValue(reader, \"{c.ColumnName}\")) == \"1\";";
             }
 
-            return $"{c.ColumnName.ToContractName()}= DataReaderUtil.{c.SqlReaderMethod}(DataReaderUtil.ReadValue(reader, \"{c.ColumnName}\"))";
+            return $"{c.ColumnName.ToContractName()}= DataReaderUtil.{c.SqlReaderMethod}(DataReaderUtil.ReadValue(reader, \"{c.ColumnName}\"));";
         }
         #endregion
     }
