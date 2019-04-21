5ea35c002b3deef55bdabba77bc81a1fa36d9708
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-21 21:43:13 +0300

    .

--- BOA.HelperTools/Tests/BOA.CodeGeneration.Test/InitializeDatabase.sql
+++ BOA.HelperTools/Tests/BOA.CodeGeneration.Test/InitializeDatabase.sql
@@ -6,6 +6,11 @@
 
 
 
+
+
+
+
+
 IF NOT EXISTS ( SELECT  *
                 FROM    sys.schemas
                 WHERE   name = N'TST' ) 
@@ -33,12 +38,25 @@ CREATE TABLE TST.Table1
 	Value4 DATETIME NULL,
 	Value5 DATETIME,
 	Value6 INT NULL,
-	Value7 INT
+	Value7 INT,
+	Value8 INT
 );
 
 
 CREATE UNIQUE INDEX index_1
 ON TST.Table1 (Value7);
 
+
+
+
+
 CREATE INDEX index_2
 ON TST.Table1 (Value0);
+
+
+CREATE UNIQUE INDEX index_3
+ON TST.Table1 (Value3,Value4);
+
+
+CREATE INDEX index_4
+ON TST.Table1 (Value7,Value8);
