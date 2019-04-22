98faaa88dfd51c895ddf6890e0583c7cf98991f5
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-22 09:05:14 +0300

    .

--- BOA.HelperTools/BOA.CodeGeneration/Contracts/Dao/SchemaInfoDao.cs
+++ BOA.HelperTools/BOA.CodeGeneration/Contracts/Dao/SchemaInfoDao.cs
@@ -5,6 +5,30 @@ namespace BOA.CodeGeneration.Contracts.Dao
 {
     public static class SchemaInfoDao
     {
+        public static IReadOnlyList<string> GetAllUserCreatedSchemaNames(IDatabase database)
+        {
+            var sql = $@"
+
+SELECT name FROM sys.schemas WHERE principal_id = 1 and name<>'dbo'
+
+";
+            database.CommandText = sql;
+
+            var tableNames = new List<string>();
+
+            var reader = database.ExecuteReader();
+            while (reader.Read())
+            {
+                tableNames.Add(reader["name"].ToString());
+            }
+
+            reader.Close();
+
+            return tableNames;
+        }
+
+
+        
         #region Public Methods
         public static IReadOnlyList<string> GetAllTableNamesInSchema(IDatabase database, string schema)
         {
