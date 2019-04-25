1c71d1a4eb5aba673dd81c0c357459761ef017ed
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-25 14:04:35 +0300

    .

---
+++ BOA.EntityGeneration/ScriptModelCreation/SelectByUniqueIndexInfoCreator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BOA.Common.Helpers;
+using BOA.EntityGeneration.Common;
+using BOA.EntityGeneration.DbModel;
+using BOA.EntityGeneration.ScriptModel;
+
+namespace BOA.EntityGeneration.ScriptModelCreation
+{
+    public static class SelectByUniqueIndexInfoCreator
+    {
+        #region Public Methods
+        public static SelectByUniqueIndexInfo Create(TableInfo tableInfo, IndexIdentifier indexIdentifier)
+        {
+            var sqlParameters = tableInfo.Columns.Where(x => indexIdentifier.IndexInfo.ColumnNames.Contains(x.ColumnName)).ToList();
+
+            return new SelectByUniqueIndexInfo
+            {
+                Sql           = GetSql(tableInfo, sqlParameters),
+                SqlParameters = sqlParameters
+            };
+        }
+        #endregion
+
+        #region Methods
+        static string GetSql(TableInfo tableInfo, IReadOnlyList<ColumnInfo> sqlParameters)
+        {
+            var sb = new PaddedStringBuilder();
+
+            sb.AppendLine("SELECT");
+            sb.PaddingCount++;
+
+            sb.AppendAll(string.Join("," + Environment.NewLine, tableInfo.Columns.Select(c => $"[{c.ColumnName}]")));
+            sb.AppendLine();
+            sb.AppendLine($"FROM [{tableInfo.SchemaName}].[{tableInfo.TableName}] WITH (NOLOCK)");
+            sb.PaddingCount--;
+            sb.AppendLine("WHERE");
+            sb.PaddingCount++;
+
+            sb.AppendAll(string.Join(" AND " + Environment.NewLine, sqlParameters.Select(c => $"[{c.ColumnName}] = @{c.ColumnName}")));
+            sb.AppendLine();
+            sb.PaddingCount--;
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
