14b26714c5abdd0620845083c231c5094a436492
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-28 00:27:52 +0300

    .

---
+++ BOA.TfsAccess.Test/TFSAccessForBOATest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.TfsAccess.Test
+{
+    [TestClass]
+    public class TFSAccessForBOATest
+    {
+        #region Public Methods
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        #endregion
+    }
+}
