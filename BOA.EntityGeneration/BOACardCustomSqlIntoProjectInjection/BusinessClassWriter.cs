30a8cb1874f9d7900ba75fcb0e6878d29a9cf834
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-04-28 06:44:03 +0300

    .

--- BOA.EntityGeneration/BOACardCustomSqlIntoProjectInjection/BusinessClassWriter.cs
+++ BOA.EntityGeneration/BOACardCustomSqlIntoProjectInjection/BusinessClassWriter.cs
@@ -1,50 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using BOA.Common.Helpers;
+﻿using BOA.Common.Helpers;
 
 namespace BOA.EntityGeneration.BOACardCustomSqlIntoProjectInjection
 {
-    [Serializable]
-    public class CustomSqlInfo
-    {
-        #region Public Properties
-        public string Name { get; set; }
-
-        public string                                ParameterContractName => Name.ToContractName() + "Request";
-        public IReadOnlyList<CustomSqlInfoParameter> Parameters            { get; set; }
-        public IReadOnlyList<CustomSqlInfoResult>    ResultColumns         { get; set; }
-
-        public string ResultContractName => Name.ToContractName() + "Contract";
-        public string Sql                { get; set; }
-        #endregion
-    }
-
-    [Serializable]
-    public class CustomSqlInfoParameter
-    {
-        #region Public Properties
-        public string DataType         { get; set; }
-        public string DataTypeInDotnet { get; set; }
-        public string Name             { get; set; }
-
-        public string NameInDotnet { get; set; }
-        #endregion
-    }
-
-    [Serializable]
-    public class CustomSqlInfoResult
-    {
-        #region Public Properties
-        public string DataType { get; set; }
-
-        public string DataTypeInDotnet { get; set; }
-        public string Name             { get; set; }
-
-        public string NameInDotnet { get; set; }
-        #endregion
-    }
-
-    public class TypeWriter
+    public class BusinessClassWriter
     {
         #region Public Methods
         public void Write(PaddedStringBuilder sb, CustomSqlInfo data)
