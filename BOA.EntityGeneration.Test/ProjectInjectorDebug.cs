d7aaa22f60c95cdf61a1b5cd9912bf4e5ccdbec3
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-05-01 19:10:23 +0300

    .

--- BOA.EntityGeneration.Test/ProjectInjectorDebug.cs
+++ BOA.EntityGeneration.Test/ProjectInjectorDebug.cs
@@ -13,7 +13,23 @@ namespace BOA.EntityGeneration.SchemaToDllExporting
         {
             using (var kernel = new Kernel())
             {
-                kernel.Get<ProjectInjector>().Inject("CC_OPERATIONS");
+                kernel.Get<ProjectInjector>().Inject("ACQUIRING");
+                //kernel.Get<ProjectInjector>().Inject("ACQUIRING_APPLICATION");
+                //kernel.Get<ProjectInjector>().Inject("BKM_MANAGEMENT");
+                //kernel.Get<ProjectInjector>().Inject("CARD_APPLICATION");
+                //kernel.Get<ProjectInjector>().Inject("CC_OPERATIONS");
+                //kernel.Get<ProjectInjector>().Inject("CLEARING_COMMON");
+                //kernel.Get<ProjectInjector>().Inject("CRD_INSTANT");
+                //kernel.Get<ProjectInjector>().Inject("CRD_ISSUING");
+                //kernel.Get<ProjectInjector>().Inject("CRD_MANAGEMENT");
+                //kernel.Get<ProjectInjector>().Inject("EMC_MANAGEMENT");
+                //kernel.Get<ProjectInjector>().Inject("PM_MANAGEMENT");
+                //kernel.Get<ProjectInjector>().Inject("SWITCH_BKM");
+                //kernel.Get<ProjectInjector>().Inject("SYS_OPERATION");
+                //kernel.Get<ProjectInjector>().Inject("SYSTEM_MANAGEMENT");
+                //kernel.Get<ProjectInjector>().Inject("TMS");
+                //kernel.Get<ProjectInjector>().Inject("VISA_MANAGEMENT");
+
             }
         }
     }
