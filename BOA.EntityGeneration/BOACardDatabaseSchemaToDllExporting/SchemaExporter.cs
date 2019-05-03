4eb93ad09d384aabb207065901142fba5f8e51ad
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-05-03 08:51:02 +0300

    .

--- BOA.EntityGeneration/BOACardDatabaseSchemaToDllExporting/SchemaExporter.cs
+++ BOA.EntityGeneration/BOACardDatabaseSchemaToDllExporting/SchemaExporter.cs
@@ -5,21 +5,39 @@ namespace BOA.EntityGeneration.BOACardDatabaseSchemaToDllExporting
 {
     public class BatExporter
     {
+        [Inject]
+        public NamingHelper NamingHelper { get; set; }
+
+        [Inject]
+        public ProjectExportLocation ProjectExportLocation { get; set; }
+
         #region Public Methods
         public void Export(string schemaName)
         {
             const string dir = @"\\srvktfs\KTBirimlerArasi\BT-Uygulama Gelistirme 3\Abdullah_Beyaztas\BOACardEntityGeneration\";
+            const string targetLocalDir = @"d:\boa\BOACard.EntityGeneration\";
+
+            var projectLocationParentDir = ProjectExportLocation.GetExportLocationOfTypeProject(schemaName);
+
+            var businessClassNamespace = NamingHelper.GetBusinessClassNamespace(schemaName);
+            var typeClassNamespace = NamingHelper.GetTypeClassNamespace(schemaName);
+
+            
+
 
             var content = $@"
 cd\
 cd windows
 cd system32
 
-robocopy ""{dir}Generator"" ""d:\boa\BOACard.EntityGeneration"" /E
+robocopy ""{dir}Generator"" ""{targetLocalDir}"" /E
 
-start D:\boa\BOACard.EntityGeneration\BOACardEntityGenerationWrapper.exe %~n0
+{targetLocalDir}BOACardEntityGenerationWrapper.exe %~n0
 
-exit
+""%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"" ""{projectLocationParentDir}{typeClassNamespace}\{typeClassNamespace}.csproj""
+""%programfiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"" ""{projectLocationParentDir}{businessClassNamespace}\{businessClassNamespace}.csproj""
+
+if NOT [""%errorlevel%""]==[""0""] pause
 
 ";
             File.WriteAllText(dir + schemaName + ".bat", content);
@@ -78,7 +96,7 @@ exit
 
             BusinessProjectExporter.Export(schemaName, code);
 
-            BusinessDllCompiler.Compile(schemaName, code);
+            // BusinessDllCompiler.Compile(schemaName, code);
         }
 
         void ExportTypeDll(string schemaName)
@@ -87,7 +105,7 @@ exit
 
             TypesProjectExporter.Export(schemaName, code);
 
-            TypeDllCompiler.Compile(schemaName, code);
+            //TypeDllCompiler.Compile(schemaName, code);
         }
         #endregion
     }
