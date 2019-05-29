286ef18cd1d0e8a37a4c50fc0439367a3efb5126
Abdullah BEYAZTAŞ <beyaz1404@hotmail.com>
2019-05-29 18:15:14 +0300

    .

---
+++ BOA.EntityGeneration.UI/MainForm/View.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOA.EntityGeneration.UI.MainForm
+{
+    [Serializable]
+    public class Model : ModelBase
+    {
+
+    }
+
+    public class Controller : ControllerBase<Model>
+    {
+
+    }
+    public class View : WindowBase<Model, Controller>
+    {
+    }
+}
