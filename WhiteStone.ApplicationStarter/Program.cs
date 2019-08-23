4949a408574c3bcaf056f4e1d4877a040b293dc2
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-08-23 14:16:51 +0300

    .

--- WhiteStone.ApplicationStarter/Program.cs
+++ WhiteStone.ApplicationStarter/Program.cs
@@ -3,8 +3,8 @@ using System.Configuration;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Net;
+using System.Threading;
 
 namespace WhiteStone.ApplicationStarter
 {
@@ -41,8 +41,8 @@ namespace WhiteStone.ApplicationStarter
             {
                 var arguments = string.Join("|", args);
 
-                Console.WriteLine("Starting process with argument:"+arguments);
-                
+                Console.WriteLine("Starting process with argument:" + arguments);
+
                 Process.Start(Config.ProcessPath, arguments);
             }
             else
@@ -62,17 +62,17 @@ namespace WhiteStone.ApplicationStarter
             {
                 if (Directory.Exists(Config.ExportDir))
                 {
-                    Console.WriteLine("Deleting folder: "+Config.ExportDir);
+                    Console.WriteLine("Deleting folder: " + Config.ExportDir);
                     Directory.Delete(Config.ExportDir, true);
-                    Console.WriteLine("Deleted folder: "+Config.ExportDir);
+                    Console.WriteLine("Deleted folder: " + Config.ExportDir);
                 }
 
-                Console.WriteLine("Exporting zip file: "+Config.ZipFilePath);
+                Console.WriteLine("Exporting zip file: " + Config.ZipFilePath);
                 ZipFile.ExtractToDirectory(Config.ZipFilePath, Config.ExportDir);
 
-                Console.WriteLine("Deleting zip file: "+Config.ZipFilePath);
+                Console.WriteLine("Deleting zip file: " + Config.ZipFilePath);
                 File.Delete(Config.ZipFilePath);
-                Console.WriteLine("Deleted zip file: "+Config.ZipFilePath);
+                Console.WriteLine("Deleted zip file: " + Config.ZipFilePath);
             }
         }
         #endregion
@@ -103,8 +103,85 @@ namespace WhiteStone.ApplicationStarter
 
             CreateDirectoryIfNotExists(Path.GetDirectoryName(saveFilePath));
 
-            new WebClient().DownloadFile(address, saveFilePath);
+            using (var webClient = new WebClient())
+            {
+                using (var stream = webClient.OpenRead(address))
+                {
+                    if (stream == null)
+                    {
+                        throw new ArgumentException(nameof(stream));
+                    }
+
+                    stream.ReadTimeout = Timeout.Infinite;
+
+                    using (var fileStream = new FileStream(saveFilePath, FileMode.OpenOrCreate))
+                    {
+                        stream.TransferStream(fileStream, trace => Console.WriteLine(trace.ShuttleCount));
+                    }
+                }
+            }
         }
+
+        /// <summary>
+        ///     Transfers the stream.
+        /// </summary>
+        public static void TransferStream(this Stream inputStream, Stream outputStream, Action<StreamReadTrace> onRead)
+        {
+            if (inputStream == null)
+            {
+                throw new ArgumentNullException(nameof(inputStream));
+            }
+
+            if (outputStream == null)
+            {
+                throw new ArgumentNullException(nameof(outputStream));
+            }
+
+            if (onRead == null)
+            {
+                throw new ArgumentNullException(nameof(onRead));
+            }
+
+            var trace = new StreamReadTrace();
+
+            var array = new byte[2048];
+            int count;
+            do
+            {
+                count = inputStream.Read(array, 0, array.Length);
+
+                outputStream.Write(array, 0, count);
+
+                trace.ShuttleCount++;
+                trace.CurrentByteCount =  count;
+                trace.TotalByteCount   += count;
+
+                onRead.Invoke(trace);
+            } while (count > 0);
+        }
+        #endregion
+    }
+
+    /// <summary>
+    ///     The stream read trace
+    /// </summary>
+    public class StreamReadTrace
+    {
+        #region Public Properties
+        /// <summary>
+        ///     Gets the current byte count.
+        /// </summary>
+        public int CurrentByteCount { get; internal set; }
+
+        /// <summary>
+        ///     Gets the shuttle count.
+        /// </summary>
+        public int ShuttleCount { get; internal set; }
+
+        /// <summary>
+        ///     Gets the total byte count.
+        /// </summary>
+        public int TotalByteCount { get; internal set; }
         #endregion
     }
 }
--- WhiteStone/Helpers/StreamUtility.cs
+++ WhiteStone/Helpers/StreamUtility.cs
@@ -4,15 +4,77 @@ using System.IO;
 namespace WhiteStone.Helpers
 {
     /// <summary>
+    ///     The stream read trace
+    /// </summary>
+    public class StreamReadTrace
+    {
+        #region Public Properties
+        /// <summary>
+        ///     Gets the current byte count.
+        /// </summary>
+        public int CurrentByteCount { get; internal set; }
+
+        /// <summary>
+        ///     Gets the shuttle count.
+        /// </summary>
+        public int ShuttleCount { get; internal set; }
+
+        /// <summary>
+        ///     Gets the total byte count.
+        /// </summary>
+        public int TotalByteCount { get; internal set; }
+        #endregion
+    }
+
+    /// <summary>
     ///     Helper extension methods for Stream class.
     /// </summary>
     public static class StreamUtility
     {
+        #region Public Methods
+        /// <summary>
+        ///     Reads all stream and returns as Byte Array
+        /// </summary>
+        public static byte[] ReadAll(this Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            var array = new byte[stream.Length];
+            stream.Read(array, 0, (int) stream.Length);
+            return array;
+        }
+
+        /// <summary>
+        ///     Reads all write to output.
+        /// </summary>
+        public static void ReadAllWriteToOutput(this Stream inputStream, Stream outputStream)
+        {
+            if (inputStream == null)
+            {
+                throw new ArgumentNullException(nameof(inputStream));
+            }
+
+            if (outputStream == null)
+            {
+                throw new ArgumentNullException(nameof(outputStream));
+            }
+
+            var array = new byte[2048];
+            int count;
+            do
+            {
+                count = inputStream.Read(array, 0, array.Length);
+
+                outputStream.Write(array, 0, count);
+            } while (count > 0);
+        }
+
         /// <summary>
         ///     Reads all part of stream and returns as String
         /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
         public static string ReadToEndAsString(this Stream stream)
         {
             if (stream == null)
@@ -27,43 +89,42 @@ namespace WhiteStone.Helpers
         }
 
         /// <summary>
-        ///     Reads all stream and returns as Byte Array
+        ///     Transfers the stream.
         /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
-        public static byte[] ReadAll(this Stream stream)
-        {
-            if (stream == null)
-            {
-                throw new ArgumentNullException("stream");
-            }
-            var array = new byte[stream.Length];
-            stream.Read(array, 0, (int) stream.Length);
-            return array;
-        }
-
-        /// <summary>
-        ///     Reads all part of <paramref name="inputStream" /> then writes to <paramref name="outputStream" />
-        /// </summary>
-        /// <param name="inputStream"></param>
-        /// <param name="outputStream"></param>
-        public static void ReadAllWriteToOutput(this Stream inputStream, Stream outputStream)
+        public static void TransferStream(this Stream inputStream, Stream outputStream, Action<StreamReadTrace> onRead)
         {
             if (inputStream == null)
             {
-                throw new ArgumentNullException("inputStream");
+                throw new ArgumentNullException(nameof(inputStream));
             }
+
             if (outputStream == null)
             {
-                throw new ArgumentNullException("outputStream");
+                throw new ArgumentNullException(nameof(outputStream));
             }
+
+            if (onRead == null)
+            {
+                throw new ArgumentNullException(nameof(onRead));
+            }
+
+            var trace = new StreamReadTrace();
+
             var array = new byte[2048];
-            int num;
+            int count;
             do
             {
-                num = inputStream.Read(array, 0, array.Length);
-                outputStream.Write(array, 0, num);
-            } while (num > 0);
+                count = inputStream.Read(array, 0, array.Length);
+
+                outputStream.Write(array, 0, count);
+
+                trace.ShuttleCount++;
+                trace.CurrentByteCount =  count;
+                trace.TotalByteCount   += count;
+
+                onRead.Invoke(trace);
+            } while (count > 0);
         }
+        #endregion
     }
 }
