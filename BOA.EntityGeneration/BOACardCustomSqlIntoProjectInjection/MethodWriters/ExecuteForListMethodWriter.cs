edd4e0e9b1d07335adfb22b365732365194e1bb5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-11 15:34:29 +0300

    .

---
+++ BOA.EntityGeneration/BOACardCustomSqlIntoProjectInjection/MethodWriters/ExecuteForListMethodWriter.cs
@@ -0,0 +1,28 @@
+﻿using BOA.Common.Helpers;
+using BOA.EntityGeneration.BOACardCustomSqlIntoProjectInjection.Models.Interfaces;
+using BOA.EntityGeneration.ScriptModel;
+
+namespace BOA.EntityGeneration.BOACardCustomSqlIntoProjectInjection.MethodWriters
+{
+    static class ExecuteForListMethodWriter
+    {
+        #region Public Methods
+        public static void Write(PaddedStringBuilder sb, ICustomSqlInfo data)
+        {
+            sb.AppendLine("/// <summary>");
+            sb.AppendLine($"///{Padding.ForComment}Data access part of '{data.Name}' sql.");
+            sb.AppendLine("/// </summary>");
+            sb.AppendLine($"public GenericResponse<List<{data.ResultContractName}>> Execute({data.ParameterContractName} request)");
+            sb.AppendLine("{");
+            sb.PaddingCount++;
+
+            sb.AppendLine("var sqlInfo = CreateSqlInfo(request);");
+            sb.AppendLine();
+            sb.AppendLine($"return this.ExecuteCustomSql<{data.ResultContractName}>(CallerMemberPath, sqlInfo, ReadContract, ProfileId, ObjectId);");
+
+            sb.PaddingCount--;
+            sb.AppendLine("}");
+        }
+        #endregion
+    }
+}
