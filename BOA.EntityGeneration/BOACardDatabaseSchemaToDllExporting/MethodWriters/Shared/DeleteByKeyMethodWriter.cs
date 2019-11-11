bacde6e4302d8c60194b0842f0102a9bf68ca6b0
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-11 22:12:36 +0300

    .

---
+++ BOA.EntityGeneration/BOACardDatabaseSchemaToDllExporting/MethodWriters/Shared/DeleteByKeyMethodWriter.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using BOA.Common.Helpers;
+using BOA.EntityGeneration.ScriptModel;
+
+namespace BOA.EntityGeneration.BOACardDatabaseSchemaToDllExporting.MethodWriters.Shared
+{
+    static class DeleteByKeyMethodWriter
+    {
+        #region Public Methods
+        public static void Write(PaddedStringBuilder sb, DeleteInfo deleteInfo)
+        {
+            var parameterPart = string.Join(", ", deleteInfo.SqlParameters.Select(x => $"{x.DotNetType} {x.ColumnName.AsMethodParameter()}"));
+
+            sb.AppendLine($"static SqlInfo GetDeleteInfo({parameterPart})");
+            sb.AppendLine("{");
+            sb.PaddingCount++;
+
+            sb.AppendLine();
+            sb.AppendLine("const string sql = @\"");
+            sb.AppendAll(deleteInfo.Sql);
+            sb.AppendLine();
+            sb.AppendLine("\";");
+            sb.AppendLine();
+
+            sb.AppendLine("var sqlInfo = new SqlInfo { CommandText = sql };");
+
+            if (deleteInfo.SqlParameters.Any())
+            {
+                sb.AppendLine();
+                foreach (var columnInfo in deleteInfo.SqlParameters)
+                {
+                    sb.AppendLine($"sqlInfo.AddInParameter(\"@{columnInfo.ColumnName}\", SqlDbType.{columnInfo.SqlDbType}, {columnInfo.ColumnName.AsMethodParameter()});");
+                }
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("return sqlInfo;");
+
+            sb.PaddingCount--;
+            sb.AppendLine("}");
+        }
+        #endregion
+    }
+}
--- BOA.EntityGeneration/BOACardDatabaseSchemaToDllExporting/Util/NamingHelper.cs
+++ BOA.EntityGeneration/BOACardDatabaseSchemaToDllExporting/Util/NamingHelper.cs
@@ -2,19 +2,31 @@
 
 namespace BOA.EntityGeneration.BOACardDatabaseSchemaToDllExporting.Util
 {
+    /// <summary>
+    ///     The naming helper
+    /// </summary>
     public class NamingHelper
     {
         #region Public Properties
+        /// <summary>
+        ///     Gets or sets the configuration.
+        /// </summary>
         [Inject]
         public Config Config { get; set; }
         #endregion
 
         #region Public Methods
+        /// <summary>
+        ///     Gets the business class namespace.
+        /// </summary>
         public string GetBusinessClassNamespace(string schemaName)
         {
             return Config.DaoAssemblyNamespaceFormat.Replace("{SchemaName}", schemaName);
         }
 
+        /// <summary>
+        ///     Gets the type class namespace.
+        /// </summary>
         public string GetTypeClassNamespace(string schemaName)
         {
             return Config.EntityAssemblyNamespaceFormat.Replace("{SchemaName}", schemaName);
