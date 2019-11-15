9452bbc966129b6942c08c28866c2eccd307d27d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-15 16:53:08 +0300

    .

--- BOA.EntityGeneration/CustomSQLExporting/Exporters/CustomSqlDataContextCreator.cs
+++ BOA.EntityGeneration/CustomSQLExporting/Exporters/CustomSqlDataContextCreator.cs
@@ -21,6 +21,7 @@ namespace BOA.EntityGeneration.CustomSQLExporting.Exporters
         {
 
             TypeFileExporter.AttachEvents(context);
+            SharedFileExporter.AttachEvents(context);
             BoaRepositoryFileExporter.AttachEvents(context);
 
             
