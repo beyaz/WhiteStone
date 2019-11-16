1b372a0d06be42744f8a9f1ba7bcdd962862a992
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-16 16:43:22 +0300

    .

---
+++ WhiteStone.Test/Common.Helpers/ConfigurationDictionaryCompilerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.Common.Helpers
+{
+    [TestClass]
+    public class ConfigurationDictionaryCompilerTest
+    {
+        [TestMethod]
+        public void Compile()
+        {
+
+            var map = new Dictionary<string, string>
+            {
+                {"A", "B"},
+                {"A2", "t$(A)"},
+                {"A3", "9"},
+                {"A4", "9$(A)-p-$(A2)"}
+            };
+
+            var compiledMap = ConfigurationDictionaryCompiler.Compile(map);
+
+            compiledMap.Should().BeEquivalentTo(new Dictionary<string, string>
+            {
+                {"A", "B"},
+                {"A2", "tB"},
+                {"A3", "9"},
+                {"A4", "9B-p-tB"}
+            });
+
+        }
+    }
+}
---
+++ WhiteStone/BOA.Common.Helpers/ConfigurationDictionaryCompiler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+
+namespace BOA.Common.Helpers
+{
+    public static class ConfigurationDictionaryCompiler
+    {
+        #region Public Methods
+        public static IDictionary<string, string> Compile(IDictionary<string, string> dictionary)
+        {
+            var pairs = new List<Pair>();
+
+            foreach (var pair in dictionary)
+            {
+                pairs.Add(new Pair {key = pair.Key, value = pair.Value});
+            }
+
+            foreach (var pair in pairs)
+            {
+                Apply(pairs, pair.key, pair.value);
+            }
+
+            var outputDictionary = new Dictionary<string, string>();
+
+            foreach (var pair in pairs)
+            {
+                outputDictionary.Add(pair.key, pair.value);
+            }
+
+            return outputDictionary;
+        }
+        #endregion
+
+        #region Methods
+        static void Apply(List<Pair> pairs, string key, string value)
+        {
+            foreach (var pair in pairs)
+            {
+                if (pair.value == null)
+                {
+                    continue;
+                }
+
+                if (pair.key == key)
+                {
+                    continue;
+                }
+
+                pair.value = pair.value.Replace($"$({key})", value);
+            }
+        }
+        #endregion
+
+        class Pair
+        {
+            #region Fields
+            public string key, value;
+            #endregion
+        }
+    }
+}
