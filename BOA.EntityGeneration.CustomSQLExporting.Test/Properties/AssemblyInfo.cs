d471629a10befec2883e50bf9bf3e41b64ea8bbd
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-16 20:20:02 +0300

    .

---
+++ BOA.EntityGeneration.CustomSQLExporting.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BOA.EntityGeneration.CustomSQLExporting.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BOA.EntityGeneration.CustomSQLExporting.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("074ac9d0-29cb-48e7-a6ca-3c0d7cbc5475")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
