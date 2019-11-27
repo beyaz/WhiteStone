c29080b515adbec33fbb15d18ff0fe190e0bd53e
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-27 14:11:35 +0300

    .

---
+++ BOA.EntityGeneration.UI.Container/CustomSqlGeneration/Components/Container.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace BOA.EntityGeneration.UI.Container.CustomSqlGeneration.Components
+{
+    public partial class Container
+    {
+        #region Fields
+        readonly Queue<string> schemaGenerationQueue = new Queue<string>();
+        #endregion
+
+        #region Constructors
+        public Container()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Public Properties
+        public string SelectedSchemaName { get; set; }
+        #endregion
+
+        #region Methods
+        void ConsumeSchemaGenerationQueue()
+        {
+            if (!schemaGenerationQueue.Any())
+            {
+                return;
+            }
+
+            StartGeneration(schemaGenerationQueue.Dequeue());
+        }
+
+        void OnGenerateClicked(object sender, RoutedEventArgs e)
+        {
+            var schemaName = SelectedSchemaName;
+
+            if (string.IsNullOrWhiteSpace(schemaName))
+            {
+                MessageBox.Show("Schema Name seçilmelidir.");
+                return;
+            }
+
+            var checkinComment = App.Model.CheckinComment;
+
+            if (string.IsNullOrWhiteSpace(checkinComment))
+            {
+                MessageBox.Show("Tfs Check-in comment girilmelidir.");
+                return;
+            }
+
+            if (schemaName == "*")
+            {
+                foreach (var item in App.Model.SchemaNames.Where(x => x != "*"))
+                {
+                    schemaGenerationQueue.Enqueue(item);
+                }
+
+                // Max three thread
+                ConsumeSchemaGenerationQueue();
+                ConsumeSchemaGenerationQueue();
+                ConsumeSchemaGenerationQueue();
+                return;
+            }
+
+            StartGeneration(schemaName);
+        }
+
+        void StartGeneration(string schemaName)
+        {
+            var ui = new SchemaGenerationProcess(schemaName);
+            ui.ProcessCompletedSuccessfully += () =>
+            {
+                Dispatcher?.Invoke(() => { processContainer.Children.Remove(ui); });
+                Dispatcher?.Invoke(ConsumeSchemaGenerationQueue);
+            };
+
+            ui.Margin = new Thickness(0, 10, 0, 0);
+
+            processContainer.Children.Add(ui);
+
+            ui.Start();
+        }
+        #endregion
+    }
+}
---
+++ BOA.EntityGeneration.UI.Container/CustomSqlGeneration/Components/SchemaGenerationProcess.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using BOA.EntityGeneration.BOACardDatabaseSchemaToDllExporting.Util;
+using BOA.EntityGeneration.SchemaToEntityExporting.Exporters;
+
+namespace BOA.EntityGeneration.UI.Container.CustomSqlGeneration.Components
+{
+    public sealed partial class SchemaGenerationProcess
+    {
+        #region Constructors
+        public SchemaGenerationProcess(string schemaName)
+        {
+            SchemaName = schemaName;
+            Process    = new ProcessContract();
+
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Public Events
+        public event Action ProcessCompletedSuccessfully;
+        #endregion
+
+        #region Public Properties
+        public ProcessContract Process    { get; set; }
+        public string          SchemaName { get; set; }
+        #endregion
+
+        #region Public Methods
+        public void Start()
+        {
+            new UIRefresher {Element = this}.Start();
+
+            new Thread(GenerateSchema).Start();
+        }
+        #endregion
+
+        #region Methods
+        void GenerateSchema()
+        {
+            var schemaExporter = new SchemaExporter();
+
+            schemaExporter.InitializeContext();
+
+            var context = schemaExporter.Context;
+
+            Process = context.processInfo;
+
+            schemaExporter.Context.FileSystem.CheckinComment                          = App.Model.CheckinComment;
+            schemaExporter.Context.FileSystem.IntegrateWithTFSAndCheckInAutomatically = context.config.IntegrateWithTFSAndCheckInAutomatically;
+
+            schemaExporter.Export(SchemaName);
+
+            if (context.Errors.Any())
+            {
+                context.processInfo.Text = string.Join(Environment.NewLine, context.Errors);
+                return;
+            }
+
+            OnProcessCompletedSuccessfully();
+        }
+
+        void OnProcessCompletedSuccessfully()
+        {
+            ProcessCompletedSuccessfully?.Invoke();
+        }
+        #endregion
+    }
+}
