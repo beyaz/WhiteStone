5837f45baed6e6017de7020906ced82c9b6b252f
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-11-29 12:34:59 +0300

    .

---
+++ WhiteStone/BOA.Common.Helpers/YamlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using YamlDotNet.Serialization;
+
+namespace BOA.Common.Helpers
+{
+    public static class YamlHelper
+    {
+        #region Public Methods
+        public static T Deserialize<T>(string serializedContent)
+        {
+            return new Deserializer().Deserialize<T>(serializedContent);
+        }
+
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            return Deserialize<T>(File.ReadAllText(filePath));
+        }
+        #endregion
+    }
+}
