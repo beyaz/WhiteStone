a21e723c02ea55b13f2e18e8c2f11d10ba54bb11
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-01 16:15:07 +0300

    .

--- BOA.EntityGeneration.SchemaToEntityExporting.Test/SampleDatabaseTest.cs
+++ BOA.EntityGeneration.SchemaToEntityExporting.Test/SampleDatabaseTest.cs
@@ -1,7 +1,11 @@
-﻿using BOA.Common.Helpers;
+﻿using System;
+using BOA.Business.Kernel.Card.ERP.Repository;
+using BOA.Common.Helpers;
+using BOA.DatabaseAccess;
 using BOA.EntityGeneration.SchemaToEntityExporting.Exporters;
 using BOA.EntityGeneration.SchemaToEntityExporting.FileExporters.AllSchemaInOneClassRepositoryFile;
 using BOA.EntityGeneration.SchemaToEntityExporting.FileExporters.EntityFileExporting;
+using BOA.Types.Kernel.Card.ERP;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,6 +31,49 @@ namespace BOA.EntityGeneration.SchemaToEntityExporting
             schemaExporter.Export("ERP");
 
             schemaExporter.Context.ErrorList.Should().BeEmpty();
+
+            TestDmlOperations(schemaExporter.Database);
+        }
+        #endregion
+
+        #region Methods
+        static void TestDmlOperations(IDatabase database)
+        {
+            var repository = new ERPRepository(database);
+
+            var contract = new SampleTableContract
+            {
+                FieldVarbinary = new byte[]{8},
+                FieldChar = "y",
+                FieldNchar = "t",
+                FieldVarchar50 = "y",
+                FieldNvarchar = "5",
+                FieldVarchar50Nullable = "y",
+                RowGuid = "y",
+                FieldSmalldatetime         = DateTime.Today,
+                FieldDatetime              = DateTime.Now,
+                FieldDatetimeNullable = DateTime.Today,
+                FieldNumeric270Nullable = 4,
+                FieldIntNullable = 5,
+                FieldMoneyNullable = 57.7M,
+                FieldNvarcharNullable = "6y",
+                FieldNcharNullable = "4",
+                FieldSmalldatetimeNullable = DateTime.Today,
+                FieldBigintNullable = 6,
+                FieldBitNullable = true,
+                FieldCharNullable = "1",
+                FieldDecimalNullable = 3,
+                FieldSmallintNullable = 3,
+                FieldTinyintNullable = 5,
+                FieldUniqueidentifierNullable = new Guid(),
+                FieldVarbinaryNullable = new byte[]{8},
+
+            };
+
+
+            repository.Insert(contract);
+
+            contract.SampleTableId.Should().Be(1);
         }
         #endregion
     }
