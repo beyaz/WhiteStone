04e8a3ec50b4df7fd534d2763e1bd129a87ff57d
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-02 16:39:20 +0300

    .

--- BOA.EntityGeneration.CustomSQLExporting/Wrapper/CustomSqlExporterConfig.cs
+++ BOA.EntityGeneration.CustomSQLExporting/Wrapper/CustomSqlExporterConfig.cs
@@ -14,6 +14,32 @@ namespace BOA.EntityGeneration.CustomSQLExporting.Wrapper
         public CustomSqlNamingPatternContract CustomSqlNamingPattern { get; set; }
             
 
+        /// <summary>
+        ///     Gets or sets the connection string.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the custom SQL get SQL item information.
+        /// </summary>
+        public string CustomSQL_Get_SQL_Item_Info { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the custom SQL names defined to profile SQL.
+        /// </summary>
+        public string CustomSQLNamesDefinedToProfileSql { get; set; }
+
+
+        /// <summary>
+        ///     Gets or sets the name of the database enum.
+        /// </summary>
+        public string DatabaseEnumName { get; set; }
+
+        
+        /// <summary>
+        ///     Gets or sets the SQL get profile identifier list.
+        /// </summary>
+        public string SQL_GetProfileIdList { get; set; }
 
         public static CustomSqlExporterConfig CreateFromFile(string filePath)
         {
