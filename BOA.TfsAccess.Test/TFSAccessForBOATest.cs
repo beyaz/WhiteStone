e0b6aa0e31f08df0067e4f80dc040f85e6d579f7
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-04 08:29:47 +0300

    .

--- BOA.TfsAccess.Test/TFSAccessForBOATest.cs
+++ BOA.TfsAccess.Test/TFSAccessForBOATest.cs
@@ -1,11 +1,81 @@
 ﻿using BOA.CodeGeneration.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace BOA.TfsAccess.Test
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BOA.TfsAccess
 {
     [TestClass]
     public class TFSAccessForBOATest
     {
+        #region Properties
+        static TFSAccessForBOA Api => new TFSAccessForBOA();
+        #endregion
+
+        #region Public Methods
+        [TestMethod]
+        public void CheckInFile()
+        {
+            // ARRANGE
+            var path = @"D:\work\BOA.Retired\Dev\BOA.Kernel.DataAccess\BOA.CodeGeneration\Common\SqlReaderMethods.cs";
+
+            TFSAccessForBOA.CheckoutFile(path);
+
+            // ACT  + ASSERT
+            //TFSAccessForBOA.CheckInFile(path, "INC292087# auto check in.");
+        }
+
+        [TestMethod]
+        public void CheckInSolution()
+        {
+            var input = new CheckInSolutionInput
+            {
+                Comment          = "583# - Can Enter new Debit provizyon bilgisinin kaldırılması.",
+                SolutionFilePath = @"D:\work\BOA.Kernel\Dev\BOA.Kernel.CardGeneral\BOA.Kernel.CardGeneral.sln"
+            };
+
+            // TFSAccessForBOA.CheckInSolution(input);
+        }
+
+        [TestMethod]
+        public void CheckOutFile()
+        {
+            // ARRANGE
+            var path = @"D:\work\BOA.Retired\Dev\BOA.Kernel.DataAccess\BOA.CodeGeneration\Common\SqlReaderMethods.cs";
+            // path = @"D:\workde\BOA.BusinessModules\Dev\BOA.Card.CreditCardOperation\BOA.Business.Card.CreditCardOperation\BOA.Business.Kernel.CreditCard\CreditCardInstallmentTransaction.designer.cs";
+
+            TFSAccessForBOA.CheckoutFile(path).Should().BeNull();
+            TFSAccessForBOA.UndoCheckoutFile(path).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void GetFileContent()
+        {
+            // ARRANGE
+            var path = @"$/BOA.BusinessModules/Dev/BOA.CardPaymentSystem.Clearing/BOA.Types.CardPaymentSystem.Clearing/FormAssistant.cs";
+
+            // ACT
+            var content = TFSAccessForBOA.GetFileContent(path);
+
+            // ASSERT
+            Assert.IsNotNull(content);
+        }
+        [TestMethod]
+        public void GetSubFolderNames()
+        {
+            // ARRANGE
+            const string tfsPathWithSearchPattern = @"$/BOA.BusinessModules/Dev/*";
+
+            // ACT
+            var content = TFSAccessForBOA.GetSubFolderNames(tfsPathWithSearchPattern);
+
+            // ASSERT
+            Assert.IsNotNull(content);
+        }
+
+        #endregion
+
         #region Public Methods
         // [TestMethod]
         public void CreateWorkspace()
@@ -13,5 +83,9 @@ namespace BOA.TfsAccess.Test
             TFSAccessForBOA.CreateWorkspace(TFSAccessForBOA.KT,"BT3UG105NB2","$/",@"D:\work");
         }
         #endregion
+
     }
 }
+
+
+
