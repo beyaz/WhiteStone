e3d9df2c53d9df256fd67d5339f02a941ed5fc21
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-04 16:55:43 +0300

    .

--- BOA.EntityGeneration.UI.Container/BuildOutput.bat
+++ BOA.EntityGeneration.UI.Container/BuildOutput.bat
@@ -13,6 +13,16 @@ Set zipFilePath=EntityGenerator.zip
 :: create zip file
 %sevenZip% a %zipFilePath% %sourceDirctory%BOA.EntityGeneration.UI.Container.exe
 :: push files
+%sevenZip% u %zipFilePath% %sourceDirctory%BOA.TfsAccess.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%WhiteStone.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.SchemaToEntityExporting.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.CustomSQLExporting.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.ConstantsProjectGeneration.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%MahApps.Metro.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%ControlzEx.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%System.Windows.Interactivity.dll
+%sevenZip% u %zipFilePath% %sourceDirctory%WpfControls.dll
 %sevenZip% u %zipFilePath% %sourceDirctory%Exporters
 %sevenZip% u %zipFilePath% %sourceDirctory%FileExporters
 
