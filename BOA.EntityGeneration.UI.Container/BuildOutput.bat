5260e93d78adaf984b433d68be268b442f46e0d5
Abdullah Beyaztaş <beyaz1404@gmail.com>
2019-12-04 17:09:18 +0300

    .

--- BOA.EntityGeneration.UI.Container/BuildOutput.bat
+++ BOA.EntityGeneration.UI.Container/BuildOutput.bat
@@ -10,23 +10,37 @@ Set sourceDirctory=%CD%\bin\debug\
 Set zipFilePath=EntityGenerator.zip
 
 
+:: Wait one second between each operations for avoid .tmp file locks.
+
 :: create zip file
 %sevenZip% a %zipFilePath% %sourceDirctory%BOA.EntityGeneration.UI.Container.exe
+timeout 1
 :: push files
 %sevenZip% u %zipFilePath% %sourceDirctory%BOA.TfsAccess.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%WhiteStone.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.SchemaToEntityExporting.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.CustomSQLExporting.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.ConstantsProjectGeneration.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%BOA.EntityGeneration.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%MahApps.Metro.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%ControlzEx.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%System.Windows.Interactivity.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%WpfControls.dll
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%Exporters
+timeout 1
 %sevenZip% u %zipFilePath% %sourceDirctory%FileExporters
+timeout 1
 
 @echo Success
-@echo Success
-@echo Success
+
 pause
